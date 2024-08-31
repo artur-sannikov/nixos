da07cfdfbe258f0ba72f34932caa18146fa59099
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-08-31 10:56:00 +0300

    Initial commit

---
+++ disko.nix
@@ -0,0 +1,49 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/vda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              size = "1000M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                subvolumes = {
+                  "/root" = {
+                    mountpoint = "/";
+                  };
+                  "/home" = {
+                    mountOptions = [ "compress=zstd" ];
+                    mountpoint = "/home";
+                  };
+                  "/nix" = {
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                    mountpoint = "/nix";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+    # home-manager, used for managing user configuration
+    home-manager = {
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+    username = "artur";
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    nixosConfigurations = {
+      vm-ty = nixpkgs.lib.nixosSystem {
+        inherit system;
+        # specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          inputs.disko.nixosModules.disko
+
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users."${username}" = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+            home-manager.extraSpecialArgs = { inherit inputs; };
+          }
+        ];
+      };
+        ty = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./hosts/ty.nix
+#             nix-flatpak.homeManagerModules.nix-flatpak
+#             {
+#               nixpkgs.overlays = [
+#                 nix-vscode-extensions.overlays.default
+#               ];
+#             }
+          ];
+        };
+    };
+  };
+}
