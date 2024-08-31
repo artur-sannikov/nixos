04760a0e24aa8afa85eab4c93f54ee8cec107ebf
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-08-31 14:45:41 +0300

    del: bash options

--- modules/home-manager/cli/bash.nix
+++ modules/home-manager/cli/bash.nix
@@ -4,40 +4,11 @@
   pkgs,
   ...
 }:
-
-let
-  inherit (lib)
-    types
-    mkOption
-    attrsOf
-    mkEnableOption
-    ;
-
-  cfg = config.nixos.bash;
-
-  userOpts = {
-    options = {
-      enable = mkEnableOption "bash" // {
-        default = true;
-      };
-    };
-  };
-in
 {
-
-  options.nixos.bash =
-    with types;
-    mkOption {
-      description = "Enable bash.";
-      type = attrsOf (submodule userOpts);
-      default = { };
-    };
-  config = {
-    programs = {
-      bash = {
-        enable = true;
-        historySize = 10000;
-      };
+  programs = {
+    bash = {
+      enable = true;
+      historySize = 10000;
     };
   };
 }
