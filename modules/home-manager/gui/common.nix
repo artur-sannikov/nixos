eb365a2236668ccf458635c7043fed615001d7ac
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-09-02 12:35:34 +0300

    del: hunspell personal

--- modules/home-manager/gui/common.nix
+++ modules/home-manager/gui/common.nix
@@ -8,7 +8,6 @@
 
   home.packages =
     (with pkgs; [
-      hunspell-personal
       betterbird
       obsidian
       vlc
@@ -18,65 +17,13 @@
       # protonvpn-gui
       # freetube
       libreoffice-qt6-fresh
-      # hunspell
-      # hunspellDicts.en_GB-large
+      hunspell
+      hunspellDicts.en_GB-large
       onlyoffice-bin
     ])
     ++ (with pkgs-unstable; [
       zotero
     ]);
-  nixpkgs.overlays = [
-    (final: prev: rec {
-      hunspell-personal =
-        with prev;
-        let
-          personal-langs = with hunspellDicts; [
-            # de_DE
-            # en_US-large
-            (stdenv.mkDerivation rec {
-              pname = "hunspell-dict-nl-opentaal";
-              version = "2.20.19";
-              src = fetchFromGitHub {
-                owner = "OpenTaal";
-                repo = "opentaal-hunspell";
-                rev = "${version}";
-                sha256 = "sha256-/rYufNGkXWH7UQDa8ZI55JtP9LM+0j7Pad8zm2tFqko=";
-              };
-              meta = {
-                description = "Dutch dictionary for spelling checker Hunspell and Nuspell";
-                homepage = "https://github.com/OpenTaal/opentaal-hunspell";
-                license = {
-                  fullName = "Revised BSD License and/or CC BY 3.0";
-                  url = "https://github.com/OpenTaal/opentaal-hunspell/blob/${version}/LICENSE.txt";
-                };
-                maintainers = with maintainers; [ ];
-                platforms = lib.platforms.all;
-              };
-
-              dictFileName = "nl";
-              installPhase = ''
-                # hunspell dicts
-                install -dm755 "$out/share/hunspell"
-                install -m644 ${dictFileName}.dic "$out/share/hunspell/"
-                install -m644 ${dictFileName}.aff "$out/share/hunspell/"
-
-                # myspell dicts symlinks
-                install -dm755 "$out/share/myspell/dicts"
-                ln -sv "$out/share/hunspell/${dictFileName}.dic" "$out/share/myspell/dicts/"
-                ln -sv "$out/share/hunspell/${dictFileName}.aff" "$out/share/myspell/dicts/"
-                runHook postInstall
-              '';
-            })
-          ];
-        in
-        (hunspellWithDicts personal-langs).overrideAttrs (old: {
-          buildCommand = ''
-            ${old.buildCommand}
-            cp -r ${hunspell.man}/share $out/share
-          '';
-        });
-    })
-  ];
   # GUI apps with special settings
   imports = [
     ./browsers/firefox.nix
