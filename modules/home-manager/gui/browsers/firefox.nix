0f8144b519671d92a07045c191162d85ce3d6c5b
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-09-03 21:56:31 +0300

    add: more options for firefox

--- modules/home-manager/gui/browsers/firefox.nix
+++ modules/home-manager/gui/browsers/firefox.nix
@@ -3,11 +3,48 @@
   programs.firefox = {
     enable = true;
     package = pkgs.firefox-wayland;
+    # https://mozilla.github.io/policy-templates/
     policies = {
       DontCheckDefaultBrowser = true;
       DisableTelemetry = true;
       DisableFirefoxStudies = true;
       DisablePocket = true;
+
+      DisplayBookmarksToolbar = "always";
+      TranslateEnabled = true;
+
+      UserMessaging = {
+        ExtensionRecommendations = false;
+        FeatureRecommendations = false;
+        UrlbarInterventions = false;
+        SkipOnboarding = false;
+        MoreFromMozilla = false;
+      };
+
+      EnableTrackingProtection = {
+        Value = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+        EmailTracking = true;
+      };
+
+      FirefoxHome = {
+        Search = true;
+        TopSites = false;
+        SponsoredTopSites = false;
+        Highlights = false;
+        Pocket = false;
+        SponsoredPocket = false;
+        Snippets = false;
+      };
+
+      Preferences = {
+        browser.warnOnQuitShortcut = true;
+
+        # Privacy
+        browser.sessionstore.privacy_level = 2;
+      };
+
     };
     profiles = {
       default-release = {
@@ -22,7 +59,6 @@
           privateDefault = "DuckDuckGo";
         };
         settings = {
-          "browser.toolbars.bookmarks.visibility" = "always";
           "privacy.clearOnShutdown.history" = true;
           "privacy.clearOnShutdown.cookies" = true;
           "privacy.clearOnShutdown.downloads" = true;
