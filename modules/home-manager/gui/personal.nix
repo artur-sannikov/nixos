ab55c0450451a6a3d21b0fc49c324fde8b14d551
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-09-08 12:30:07 +0300

    add: rustdesk

--- modules/home-manager/gui/personal.nix
+++ modules/home-manager/gui/personal.nix
@@ -1,15 +1,23 @@
-{ pkgs-unstable, ... }:
+{ pkgs, pkgs-unstable, ... }:
 
 {
   imports = [
     ./common.nix
   ];
   # Personal apps
-  home.packages = with pkgs-unstable; [
-    audacity
-    calibre
-    protonmail-bridge-gui
-    telegram-desktop
-    signal-desktop
-  ];
+  home.packages =
+    (with pkgs-unstable; [
+      audacity
+      calibre
+      protonmail-bridge-gui
+      telegram-desktop
+      signal-desktop
+    ])
+
+    ++
+
+      (with pkgs; [
+        rustdesk
+      ]);
+
 }
