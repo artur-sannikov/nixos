6e571d820952bb737137d19d885dedb78113df0f
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-09-09 09:33:29 +0300

    add: function to grep through commits

--- modules/home-manager/cli/zsh.nix
+++ modules/home-manager/cli/zsh.nix
@@ -35,12 +35,17 @@ in
           "l" = "ls -lh";
         };
         initExtra = ''
-          # Disable underline
-          # See https://github.com/zsh-users/zsh-syntax-highlighting/issues/573
-          (( ''${+ZSH_HIGHLIGHT_STYLES} )) || typeset -A ZSH_HIGHLIGHT_STYLES
-          ZSH_HIGHLIGHT_STYLES[path]=none
-          ZSH_HIGHLIGHT_STYLES[path_prefix]=none
-          ZSH_HIGHLIGHT_STYLES[precommand]=none
+            # Disable underline
+            # See https://github.com/zsh-users/zsh-syntax-highlighting/issues/573
+            (( ''${+ZSH_HIGHLIGHT_STYLES} )) || typeset -A ZSH_HIGHLIGHT_STYLES
+            ZSH_HIGHLIGHT_STYLES[path]=none
+            ZSH_HIGHLIGHT_STYLES[path_prefix]=none
+            ZSH_HIGHLIGHT_STYLES[precommand]=none
+
+            # Function to grep through all commits
+            function git-grep-all() {
+              git grep "$1" $(git rev-list --all)
+          }
         '';
         oh-my-zsh = {
           enable = true;
