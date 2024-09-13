154d64cc2ef278cb817bc0a952ac786c5ce912c2
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-09-13 22:22:13 +0300

    harden duplicacy-web.service

--- modules/home-manager/services.nix
+++ modules/home-manager/services.nix
@@ -21,10 +21,31 @@ in
         Restart = "on-failure";
 
         # Hardening
+        DynamicUser = true;
         LockPersonality = true;
         NoNewPrivileges = true;
         MemoryDenyWriteExecute = true;
         PrivateDevices = true;
+        PrivateMounts = true;
+        PrivateTmp = true;
+        ProtectClock = true;
+        ProtectControlGroups = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectProc = "invisible";
+        ProtectSystem = "strict";
+        DevicePolicy = "closed";
+        CapabilityBoundingSet = "";
+        RemoveIPC = true;
+        RestrictAddressFamilies = "AF_UNIX AF_NETLINK AF_INET AF_INET6";
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        SystemCallArchitectures = "native";
+        SystemCallFilter = "~@clock @cpu-emulation @privileged @module @raw-io @reboot @mount @obsolete @swap @debug";
+        SocketBindDeny = "any";
+        SocketBindAllow = "tcp:3875"; # only listen on port 3875
       };
       Install = {
         WantedBy = [ "default.target" ];
