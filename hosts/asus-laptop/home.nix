2af03b4e130d096e71a80a77d3ff953c2329eac6
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-09-25 16:03:24 +0300

    change: move shadow-pc to separate file

--- hosts/asus-laptop/home.nix
+++ hosts/asus-laptop/home.nix
@@ -47,19 +47,7 @@
     ../../modules/home-manager/gui/personal.nix
     # Systemd services
     ../../modules/home-manager/services.nix
-    (
-      fetchGit {
-        url = "https://github.com/anthonyroussel/shadow-nix";
-        rev = "2063ca3633f1630bd49a24b5eef49f0d747910c5";
-      }
-      + "/import/home-manager.nix"
-    )
   ];
-
-  programs.shadow-client = {
-    enable = true;
-    channel = "prod";
-  };
 
   # Enable zsh
   zsh.enable = true;
