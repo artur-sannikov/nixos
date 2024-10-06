ba73762a4f8195ed7bf96b5cf143802ea1cd3666
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-10-06 16:32:14 +0300

    add: tmux reload shortcut

--- modules/home-manager/cli/tmux.nix
+++ modules/home-manager/cli/tmux.nix
@@ -5,6 +5,9 @@
       enable = true;
       shell = "${pkgs.zsh}/bin/zsh";
       prefix = "C-f";
+      extraConfig = ''
+        bind-key r source-file ~/.config/tmux/tmux.conf \; display-message "tmux.conf reloaded."
+      '';
     };
   };
 }
