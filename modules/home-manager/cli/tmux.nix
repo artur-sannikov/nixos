3395a82743acabddf10eeab44a636dbefaf81b11
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-10-08 21:07:22 +0300

    add: switch tmux panes without prefix key

--- modules/home-manager/cli/tmux.nix
+++ modules/home-manager/cli/tmux.nix
@@ -8,7 +8,14 @@
       extraConfig = ''
         bind-key r source-file ~/.config/tmux/tmux.conf \; display-message "tmux.conf reloaded."
 
+        # Mouse mode
         set -g mouse on
+
+        # Switch panes without prefix key
+        bind -n M-Left select-pane -L
+        bind -n M-Right select-pane -R
+        bind -n M-Up select-pane -U
+        bind -n M-Down select-pane -D
       '';
     };
   };
