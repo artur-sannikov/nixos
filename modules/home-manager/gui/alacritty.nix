80d4016999bb4fc37a976923a900e585d7661bd9
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-10-17 17:43:32 +0300

    add: start tmux on alacritty startup

--- modules/home-manager/gui/alacritty.nix
+++ modules/home-manager/gui/alacritty.nix
@@ -6,6 +6,11 @@
       settings = {
         shell = {
           program = "${pkgs.zsh}/bin/zsh";
+          args = [
+            "-l"
+            "-c"
+            "tmux attach || tmux"
+          ];
         };
         window = {
           startup_mode = "Maximized";
