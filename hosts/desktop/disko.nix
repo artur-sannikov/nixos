7c146169e40cf63a1e8855b09526d54a49704dc2
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-11-05 12:17:29 +0200

    add: desktop host

---
+++ hosts/desktop/disko.nix
@@ -0,0 +1,80 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              size = "1000M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            };
+            luks = {
+              size = "100%";
+              label = "luks";
+              content = {
+                type = "luks";
+                name = "cryptroot";
+                extraOpenArgs = [
+                  "--allow-discards"
+                  "--perf-no_read_workqueue"
+                  "--perf-no_write_workqueue"
+                ];
+                content = {
+                  type = "btrfs";
+                  extraArgs = [
+                    "-L"
+                    "nixos"
+                    "-f"
+                  ];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "subvol=root"
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = [
+                        "subvol=home"
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "subvol=nix"
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/swap" = {
+                      mountpoint = "/.swapvol";
+                      swap.swapfile.size = "2G";
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
---
+++ hosts/desktop/home.nix
@@ -0,0 +1,63 @@
+{
+  home = {
+    username = "artur";
+    homeDirectory = "/home/artur";
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "24.05";
+  };
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  # home.packages = with pkgs; [
+  #   mat2
+  #   devbox
+  #   gnumake
+  #   quarto
+  #   nixfmt-rfc-style
+  #   qt6.qtwayland
+  #   kdePackages.kate
+  # ];
+
+  # Import modules
+  imports = [
+    ../../modules/home-manager/cli/default.nix
+    ../../modules/home-manager/cli/media.nix
+    ../../modules/home-manager/cli/bash.nix
+    ../../modules/home-manager/cli/direnv.nix
+    ../../modules/home-manager/cli/git.nix
+    ../../modules/home-manager/cli/nix.nix
+    ../../modules/home-manager/cli/packages.nix
+    ../../modules/home-manager/cli/starship.nix
+    ../../modules/home-manager/cli/gnupg.nix
+    ../../modules/home-manager/cli/btop.nix
+    ../../modules/home-manager/cli/fastfetch.nix
+    ../../modules/home-manager/gui/personal.nix
+    # Systemd services
+    ../../modules/home-manager/services.nix
+
+    # Alacritty is supported on non-NixOS systems with caveats
+    ../../modules/home-manager/gui/alacritty.nix
+
+    ../../modules/home-manager/cli/tmux.nix
+
+    ../../modules/home-manager/desktop_entries.nix
+  ];
+
+  # Enable zsh
+  zsh.enable = true;
+
+  # Enable yt-dlp
+  yt-dlp.enable = true;
+
+  programs.home-manager.enable = true;
+  # Set environment variables
+  home.sessionVariables = {
+    EDITOR = "nano";
+  };
+}
