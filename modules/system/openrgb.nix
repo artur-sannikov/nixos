897b94f824c33ae3a5a902f174871050d85456e8
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-11-06 18:04:17 +0200

    add: openrgb

---
+++ modules/system/openrgb.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+{
+  services.hardware.openrgb = {
+    enable = true;
+    package = pkgs.openrgb;
+    motherboard = "amd";
+  };
+
+  # environment.systemPackages = [
+  #   pkgs.openrgb-with-all-plugins
+  # ];
+
+}
