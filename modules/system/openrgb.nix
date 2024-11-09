cca0faca47fcec1d60805d390d3387a126ac69fa
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-11-09 21:43:39 +0200

    add: openrgb-experimental

--- modules/system/openrgb.nix
+++ modules/system/openrgb.nix
@@ -1,13 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, flake-inputs, ... }:
+
 {
   services.hardware.openrgb = {
     enable = true;
-    package = pkgs.openrgb-with-all-plugins;
+    package = pkgs.openrgb.overrideAttrs {
+      version = "experimental";
+      src = flake-inputs.openrgb-experimental;
+      postPatch = ''
+        patchShebangs scripts/build-udev-rules.sh
+        substituteInPlace scripts/build-udev-rules.sh \
+          --replace "/usr/bin/env chmod" "${pkgs.coreutils}/bin/chmod"
+      '';
+    };
     motherboard = "amd";
   };
-
-  # environment.systemPackages = [
-  #   pkgs.openrgb-with-all-plugins
-  # ];
-
 }
