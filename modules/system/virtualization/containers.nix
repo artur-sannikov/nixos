ffcf37769222978227c8e13ea9bdb6115c9390c2
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-11-13 13:43:53 +0200

    add: sillytavern container

--- modules/system/virtualization/containers.nix
+++ modules/system/virtualization/containers.nix
@@ -1,14 +1,14 @@
 { username, ... }:
 {
   virtualisation.oci-containers.containers.sillytavern = {
-    image = "goolashe/sillytavern:latest";
+    image = "ghcr.io/sillytavern/sillytavern:latest";
     extraOptions = [
       "--network=host"
     ];
     ports = [ "8000" ];
     volumes = [
       "/home/${username}/podman/sillytavern/config:/home/node/app/config"
-      "/home/${username}/sillytavern/user:/home/node/app/public/user"
+      "/home/${username}/podman/sillytavern/data:/home/node/app/data"
     ];
   };
 }
