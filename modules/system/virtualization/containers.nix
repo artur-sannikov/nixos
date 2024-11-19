ef49ae8c4b786dc786b989bfd4ebc392e7debe48
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-11-19 21:29:02 +0200

    change: indentation

--- modules/system/virtualization/containers.nix
+++ modules/system/virtualization/containers.nix
@@ -1,14 +1,16 @@
 { username, ... }:
 {
-  virtualisation.oci-containers.containers.sillytavern = {
-    image = "ghcr.io/sillytavern/sillytavern:latest";
-    extraOptions = [
-      "--network=host"
-    ];
-    ports = [ "8000" ];
-    volumes = [
-      "/home/${username}/podman/sillytavern/config:/home/node/app/config"
-      "/home/${username}/podman/sillytavern/data:/home/node/app/data"
-    ];
+  virtualisation.oci-containers.containers = {
+    sillytavern = {
+      image = "ghcr.io/sillytavern/sillytavern:latest";
+      extraOptions = [
+        "--network=host"
+      ];
+      ports = [ "8000" ];
+      volumes = [
+        "/home/${username}/podman/sillytavern/config:/home/node/app/config"
+        "/home/${username}/podman/sillytavern/data:/home/node/app/data"
+      ];
+    };
   };
 }
