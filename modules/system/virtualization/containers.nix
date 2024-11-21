8baa83a29e7d831d78a260b9540e24e43eb44d6c
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-11-21 22:33:48 +0200

    add: podman folder creation

--- modules/system/virtualization/containers.nix
+++ modules/system/virtualization/containers.nix
@@ -1,12 +1,17 @@
 { username, ... }:
 {
+  systemd.tmpfiles.rules = [
+    "d /home/${username}/podman/sillytavern/config 0750 ${username} users -"
+    "d /home/${username}/podman/sillytavern/data 0750 ${username} users -"
+    "d /home/${username}/podman/comfy-ui/storage 0750 ${username} users -"
+  ];
+
   virtualisation.oci-containers.containers = {
     sillytavern = {
       image = "ghcr.io/sillytavern/sillytavern:latest";
       extraOptions = [
         "--network=host"
       ];
-      ports = [ "8000" ];
       volumes = [
         "/home/${username}/podman/sillytavern/config:/home/node/app/config"
         "/home/${username}/podman/sillytavern/data:/home/node/app/data"
