90417ac7ce0fb1d5878d997372fa430c851876bf
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-12-01 16:27:27 +0200

    del: nfs share automount from desktop

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -105,7 +105,6 @@
         device = "192.168.20.5:/mnt/tank/ux5401-backup";
         fsType = "nfs";
         options = [
-          "x-systemd.automount"
           "nofail"
           "noauto"
           "_netdev"
@@ -115,7 +114,6 @@
         device = "192.168.20.5:/mnt/tank/media";
         fsType = "nfs";
         options = [
-          "x-systemd.automount"
           "nofail"
           "noauto"
           "_netdev"
