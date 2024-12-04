bc672695f7bbeb4276ada74240889f8a674a76fe
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-12-04 10:28:54 +0200

    add: lsp-format and inlayHints to nixvim

--- modules/home-manager/nixvim.nix
+++ modules/home-manager/nixvim.nix
@@ -6,8 +6,12 @@
     colorschemes.catppuccin.enable = true;
     plugins = {
       lualine.enable = true;
+      lsp-format = {
+        enable = true;
+      };
       lsp = {
         enable = true;
+        inlayHints = true;
         servers = {
           lua_ls.enable = true;
           r_language_server = {
