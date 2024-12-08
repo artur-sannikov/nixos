91253e06a56f539b1501e3053d736634efa80353
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-12-08 13:02:58 +0200

    fix: HDR for Steam games

--- modules/system/gaming.nix
+++ modules/system/gaming.nix
@@ -22,19 +22,37 @@ in
   config = mkIf cfg.enable {
     programs = {
       steam = {
+        enable = true;
         package = pkgs.steam.override {
+          extraLibraries = pkgs: [ pkgs.xorg.libxcb ];
+          extraPkgs =
+            pkgs: with pkgs; [
+              gamescope-wsi
+            ];
           extraProfile = ''
             export STEAM_FORCE_DESKTOPUI_SCALING=${cfg.SteamUIScaling};
           '';
         };
+        # To enable HDR run Steam games with `gamemoderun gamescope --mangoapp --rt --force-grab-cursor --hdr-enabled -f -w 3840 -h 2160 --adaptive-sync --hdr-sdr-content-nits 250 --hdr-itm-target-nits 1000 -- %command%`
+        gamescopeSession = {
+          enable = true;
+          env = {
+            DXVK_HDR = "1";
+            ENABLE_GAMESCOPE_WSI = "1";
+          };
+        };
+      };
+      gamescope = {
         enable = true;
-        gamescopeSession.enable = true;
+        env = {
+          DXVK_HDR = "1";
+          ENABLE_GAMESCOPE_WSI = "1";
+        };
       };
       gamemode = {
         enable = true;
       };
     };
-
     environment = {
       systemPackages = with pkgs; [
         heroic
