32022308b88f8fe5353995450a07d4c85f956718
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-12-20 19:32:51 +0200

    add: printing

--- hosts/asus-laptop/configuration.nix
+++ hosts/asus-laptop/configuration.nix
@@ -140,6 +140,12 @@
         enable = true;
         pulse.enable = true;
       };
+      printing.enable = true;
+      avahi = {
+        enable = true;
+        nssmdns4 = true;
+        openFirewall = true;
+      };
     };
 
     security = {
