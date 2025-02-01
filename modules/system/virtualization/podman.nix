499de7120c7a7311b8980ef780eaf202b042a3c6
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-02-01 14:30:02 +0200

    change: asus-laptop fs to zfs

--- modules/system/virtualization/podman.nix
+++ modules/system/virtualization/podman.nix
@@ -1,5 +1,13 @@
 {
   virtualisation = {
+    containers.storage.settings = {
+      # defaults
+      storage = {
+        driver = "overlay";
+        graphroot = "/var/lib/containers/storage";
+        runroot = "/run/containers/storage";
+      };
+    };
     podman = {
       enable = true;
     };
