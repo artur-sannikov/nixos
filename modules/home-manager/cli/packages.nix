825cb58ba7167c16cb7f5c7f7239859c3e306b8d
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-03-11 20:47:19 +0200

    feat: add opentofu

--- modules/home-manager/cli/packages.nix
+++ modules/home-manager/cli/packages.nix
@@ -19,10 +19,11 @@
       mat2
       micromamba
       nextflow
+      opentofu
       podlet
       python312Packages.huggingface-hub
-      python312Packages.radian
       python312Packages.pip
+      python312Packages.radian
       sass
       screen
       shellcheck
