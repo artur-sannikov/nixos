0ed30d0c1a389b1aa67fa75d2e68f77201ef7c11
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-03-21 21:01:59 +0200

    chore (desktop home): make configuration more portable

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -1,7 +1,8 @@
+{ username, ... }:
 {
   home = {
-    username = "artur";
-    homeDirectory = "/home/artur";
+    username = "${username}";
+    homeDirectory = "/home/${username}";
     # This value determines the Home Manager release that your configuration is
     # compatible with. This helps avoid breakage when a new Home Manager release
     # introduces backwards incompatible changes.
