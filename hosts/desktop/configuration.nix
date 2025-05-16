99e6c39bbf1a773d83b28805883309f89a0daa93
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-05-16 19:41:43 +0300

    refactor: Delete catppuccin

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -230,11 +230,6 @@
         };
       };
     };
-
-    catppuccin = {
-      enable = true;
-      flavor = "mocha";
-    };
     # This option defines the first version of NixOS you have installed on this particular machine,
     # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
     #
