98cf6239c5ab15d3ad4f005e743425bbbb69a475
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-06-02 11:36:22 +0300

    feat: Add Zulip

--- modules/home-manager/cli/packages.nix
+++ modules/home-manager/cli/packages.nix
@@ -5,51 +5,46 @@
   ...
 }:
 {
-  home.packages =
-    with pkgs;
-    [
-      act
-      age
-      ansible
-      ansible-lint
-      black
-      beets
-      caddy
-      cloud-utils
-      fd
-      devenv
-      distrobox
-      gnumake
-      isort
-      jdk
-      kubectl
-      lynx
-      mat2
-      micromamba
-      multiqc
-      nextflow
-      opentofu
-      podlet
-      python312Packages.huggingface-hub
-      python312Packages.pip
-      python312Packages.radian
-      sass
-      screen
-      shellcheck
-      shfmt
-      sops
-      talosctl
-      terraform
-      tesseract
-      toolbox
-      typst
-      virtualenv
-      wl-clipboard-rs
-      yamlfmt
-      yamllint
-      # flake-inputs.flox.packages.${pkgs.system}.flox
-    ]
-    ++ (with pkgs-stable; [
-      # quarto
-    ]);
+  home.packages = with pkgs; [
+    act
+    age
+    ansible
+    ansible-lint
+    black
+    beets
+    caddy
+    cloud-utils
+    fd
+    devenv
+    distrobox
+    gnumake
+    isort
+    jdk
+    kubectl
+    lynx
+    mat2
+    micromamba
+    multiqc
+    nextflow
+    opentofu
+    podlet
+    python312Packages.huggingface-hub
+    python312Packages.pip
+    python312Packages.radian
+    sass
+    screen
+    shellcheck
+    shfmt
+    sops
+    talosctl
+    terraform
+    tesseract
+    toolbox
+    typst
+    virtualenv
+    wl-clipboard-rs
+    yamlfmt
+    yamllint
+    zulip
+  ];
 }
