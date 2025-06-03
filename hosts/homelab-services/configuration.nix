a5c3b07bb0386f85e4dab0faa50b032137294ca6
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-06-03 20:17:24 +0300

    refactor: Move NFS for Archivebox to archivebox.nix

--- hosts/homelab-services/configuration.nix
+++ hosts/homelab-services/configuration.nix
@@ -61,16 +61,6 @@ in
         "_netdev"
       ];
     };
-    "/mnt/nas/archivebox" = {
-      device = "192.168.20.5:/mnt/tank/personal/archivebox";
-      fsType = "nfs";
-      options = [
-        "x-systemd.automount"
-        "nofail"
-        "noauto"
-        "_netdev"
-      ];
-    };
   };
 
   # Enable Flakes
--- modules/system/virtualization/containers/archivebox.nix
+++ modules/system/virtualization/containers/archivebox.nix
@@ -19,6 +19,18 @@ in
     };
   };
   config = mkIf cfg.enable {
+    fileSystems = {
+      "/mnt/nas/archivebox" = {
+        device = "192.168.20.5:/mnt/tank/personal/archivebox";
+        fsType = "nfs";
+        options = [
+          "x-systemd.automount"
+          "nofail"
+          "noauto"
+          "_netdev"
+        ];
+      };
+    };
     virtualisation.oci-containers = {
       backend = "docker";
       containers = {
