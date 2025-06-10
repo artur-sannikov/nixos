858a97d5a9cf44190c041b9b29787f8e71468b86
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-06-10 22:44:18 +0300

    fix: Syncoid failues

--- modules/system/services/syncoid.nix
+++ modules/system/services/syncoid.nix
@@ -1,4 +1,9 @@
-{ config, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   # Secrets
   sops = {
@@ -9,23 +14,26 @@
       };
     };
   };
-
-  users.users.syncoid = {
-    isSystemUser = true;
-    createHome = false;
-    home = "/var/lib/syncoid";
-  };
   services.syncoid = {
     enable = true;
     sshKey = config.sops.secrets."keys/ssh/syncoid-ssh-key".path;
+    commonArgs = [
+      "--no-sync-snap"
+      "--debug"
+    ];
     commands = {
-      "zroot/home" = {
+      "home" = {
+        source = "zroot/home";
         target = "syncoid@192.168.20.5:tank/personal/backups/asus";
         extraArgs = [
           "--exclude=Downloads"
           "--exclude=zfs_exclude"
+          "--delete-target-snapshots" # Delete snapshots on target which are missing on source
+          "--sshoption=StrictHostKeyChecking=off"
         ];
-        recursive = true;
+        recursive = false;
+        sendOptions = "w"; # raw
+        recvOptions = "u"; # umount
       };
     };
   };
