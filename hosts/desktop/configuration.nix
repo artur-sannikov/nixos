2090c2da1b07bd4e752d2fe2f209703d3c6c9567
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-08-05 16:10:09 +0300

    feat: Set Finnish locale

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -62,6 +62,9 @@
       # Fonts
       "modules/system/fonts.nix"
 
+      # locale
+      "modules/system/locale.nix"
+
       # Import all core modules
       "modules/core/default.nix"
     ])
@@ -85,13 +88,6 @@
         };
       };
     };
-
-    i18n.defaultLocale = "en_US.UTF-8";
-
-    i18n.supportedLocales = [
-      "en_US.UTF-8/UTF-8"
-      "fi_FI.UTF-8/UTF-8"
-    ];
 
     networking = {
       hostName = "desktop";
