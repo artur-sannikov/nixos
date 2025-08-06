e22245c435b973b11d361de9715a413d155f270f
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-08-06 15:51:01 +0300

    feat (nixvim): Add shiftwidth and clipboard options

--- modules/home-manager/cli/nixvim/default.nix
+++ modules/home-manager/cli/nixvim/default.nix
@@ -6,6 +6,8 @@
     enable = true;
     opts = {
       termguicolors = true;
+      shiftwidth = 4;
+      clipboard = "unnamedplus";
     };
     extraConfigVim = ''
       syntax off "" Distable syntax highlighting, treesitter handles it
