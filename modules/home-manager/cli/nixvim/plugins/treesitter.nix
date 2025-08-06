758e4ea1ba7c52a71cf8a63b3e357640aec2ba30
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-08-06 15:48:06 +0300

    feat (neovim): Add languages to treesitter

--- modules/home-manager/cli/nixvim/plugins/treesitter.nix
+++ modules/home-manager/cli/nixvim/plugins/treesitter.nix
@@ -1,6 +1,51 @@
+{ pkgs, ... }:
 {
   programs.nixvim.plugins.treesitter = {
     enable = true;
+    grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+      bash
+      bibtex
+      caddy
+      comment
+      css
+      csv
+      desktop
+      dockerfile
+      git_config
+      git_rebase
+      gitcommit
+      gitignore
+      hcl
+      helm
+      hjson
+      html
+      ini
+      jinja
+      jinja_inline
+      jq
+      json
+      latex
+      lua
+      make
+      markdown_inline
+      nix
+      passwd
+      python
+      r
+      readline
+      regex
+      requirements # pip requirements
+      scss
+      sql
+      ssh_config
+      styled
+      terraform
+      toml
+      tsv
+      typst
+      xml
+      yaml
+    ];
     settings = {
       highlight = {
         enable = true;
