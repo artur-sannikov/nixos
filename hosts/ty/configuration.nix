2819d485e497b063b984af601f2b09068c3231b6
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-08-19 14:31:21 +0300

    feat: Add fwupd to ty

--- hosts/ty/configuration.nix
+++ hosts/ty/configuration.nix
@@ -30,6 +30,7 @@
       "modules/system/services/ssh.nix"
       "modules/system/services/audio.nix"
       "modules/system/services/mullvad.nix"
+      "modules/system/services/fwupd.nix"
       "modules/system/services/tailscale.nix"
       "modules/system/services/tlp.nix"
       "modules/system/services/samba.nix"
