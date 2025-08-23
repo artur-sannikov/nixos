f03244bc16163b88ac10a964f930074531157281
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-08-23 10:15:26 +0300

    fix: Librewolf profiles

--- modules/home-manager/gui/browsers/librewolf.nix
+++ modules/home-manager/gui/browsers/librewolf.nix
@@ -1,23 +1,38 @@
 { pkgs, ... }:
+let
+  settings = {
+    "browser.toolbars.bookmarks.visibility" = "always";
+    "privacy.clearOnShutdown.history" = true;
+    "privacy.clearOnShutdown.cookies" = true;
+    "privacy.clearOnShutdown.downloads" = true;
+    "privacy.clearOnShutdown.openWindows" = true;
+
+    # Enable Firefox sync
+    "identity.fxaccounts.enabled" = true;
+
+    "network.trr.mode" = 2;
+    "network.trr.uri" = "https://base.dns.mullvad.net/dns-query";
+
+    "security.ssl.enable_ocsp_must_staple" = false;
+
+    "browser.tabs.insertAfterCurrent" = true;
+    "browser.tabs.warnOnClose" = true;
+  };
+in
 {
+
   programs.librewolf = {
     enable = true;
     package = pkgs.librewolf;
-    settings = {
-      "browser.toolbars.bookmarks.visibility" = "always";
-      "privacy.clearOnShutdown.history" = true;
-      "privacy.clearOnShutdown.cookies" = true;
-      "privacy.clearOnShutdown.downloads" = true;
-      "privacy.clearOnShutdown.openWindows" = true;
-
-      # Enable Firefox sync
-      "identity.fxaccounts.enabled" = true;
-
-      "network.trr.mode" = 2;
-      "network.trr.uri" = "https://base.dns.mullvad.net/dns-query";
-
-      "security.ssl.enable_ocsp_must_staple" = false;
+    profiles = {
+      default = { inherit settings; };
+      work = {
+        id = 1;
+        inherit settings;
+      };
     };
   };
-  stylix.targets.librewolf.profileNames = [ "default" ];
+  stylix.targets.librewolf.profileNames = [
+    "default"
+  ];
 }
