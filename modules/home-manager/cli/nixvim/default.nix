15f2e4c406686ab00de956031f87cbfb9cd95f6b
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-08-26 12:55:21 +0300

    feat: Enable numbers in neovim

--- modules/home-manager/cli/nixvim/default.nix
+++ modules/home-manager/cli/nixvim/default.nix
@@ -8,6 +8,8 @@
       termguicolors = true;
       shiftwidth = 4;
       clipboard = "unnamedplus";
+      number = true;
+      relativenumber = true;
     };
     extraConfigVim = ''
       syntax off "" Distable syntax highlighting, treesitter handles it
