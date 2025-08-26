42ec6c9e29043d894f4cb11f0df831fa9226968e
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-08-26 22:42:09 +0300

    feat: Let nixvim handle its own color scheme

--- modules/home-manager/cli/nixvim/default.nix
+++ modules/home-manager/cli/nixvim/default.nix
@@ -14,8 +14,27 @@
       spelllang = "en_us";
       formatoptions = "aw";
     };
+    colorschemes = {
+      catppuccin = {
+        enable = true;
+        settings = {
+          default_integrations = true;
+          flavour = "mocha";
+
+          # https://github.com/catppuccin/nvim/tree/main/lua/catppuccin/groups/integrations
+          integrations = {
+            cmp = true;
+            treesitter = true;
+            telescope = true;
+          };
+
+          term_colors = true;
+          transparent_background = true;
+        };
+      };
+    };
     extraConfigVim = ''
-      syntax off "" Distable syntax highlighting, treesitter handles it
+      syntax off "" Disable syntax highlighting, treesitter handles it
     '';
   };
 }
