d6a29604ff1a73ec4b6923cb6035f01d11207b10
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-09-20 15:27:18 +0300

    feat(direnv): add hide_env_diff option

--- modules/home-manager/cli/direnv.nix
+++ modules/home-manager/cli/direnv.nix
@@ -2,6 +2,11 @@
   programs = {
     direnv = {
       enable = true;
+      config = {
+        global = {
+          hide_env_diff = true;
+        };
+      };
       nix-direnv.enable = true;
       enableZshIntegration = true;
     };
