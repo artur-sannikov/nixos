df80a6e14a74648931b5323d5bbc47b4e6889c95
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-09-25 10:43:49 +0300

    feat(nixvim): add yarepl

--- modules/home-manager/cli/nixvim/plugins/yarepl.nix
+++ modules/home-manager/cli/nixvim/plugins/yarepl.nix
@@ -1,17 +1,17 @@
 { pkgs, ... }:
 {
-  programs.nixvim.plugins = {
-    yarepl = {
-      package = pkgs.vimUtils.buildVimPlugin rec {
-        pname = "yarepl.nvim";
-        version = "0.11.0";
-        src = pkgs.fetchFromGithub {
-          owner = "milanglacier";
-          repo = "yarepl.nvim";
-          rev = "v${version}";
-          hash = "";
-        };
+  programs.nixvim.extraPlugins = [
+    (pkgs.vimUtils.buildVimPlugin rec {
+      pname = "yarepl.nvim";
+      version = "0.11.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "milanglacier";
+        repo = "${pname}";
+        rev = "v${version}";
+        hash = "sha256-6kl7xQpDiyuiqmjTPVbAjff+EGIVzWw/RBOaqZP3iGo=";
       };
-    };
-  };
+      # I use telescope
+      nvimSkipModules = [ "yarepl.extensions.fzf" ];
+    })
+  ];
 }
