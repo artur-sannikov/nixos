f44968acccc7c28070b26b1eaed7c32ca9624e42
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-09-27 23:48:55 +0300

    chore(nixvim): remove old commented out code

--- modules/home-manager/cli/nixvim/plugins/lsp.nix
+++ modules/home-manager/cli/nixvim/plugins/lsp.nix
@@ -8,35 +8,6 @@
         ".*\.js\.j2" = "js";
       };
     };
-    # extraConfigLua = ''
-    #   require("blink.cmp").setup({
-    #     completion = {
-    #       enabled = function()
-    #         return not vim.tbl_contains({ "yaml.ansible" }, vim.bo.filetype)
-    #       end
-    # '';
-    # autoCmd = [
-    #   {
-    #     event = [
-    #       "BufEnter"
-    #       "BufWinEnter"
-    #     ];
-    #     # pattern = "*.yaml";
-    #     pattern = "*.yaml";
-    #     callback = {
-    #       __raw = ''
-    #         function()
-    #           if vim.bo.filetype == "yaml.ansible" then
-    #           -- if vim.bo.filetype == "python" then
-    #             -- vim.b.completion = false
-    #             require("blink.cmp.completion.menu").enabled = false
-    #             -- vim.b.completion = false
-    #           end
-    #         end
-    #       '';
-    #     };
-    #   }
-    # ];
     extraPlugins = with pkgs.vimPlugins; [
       ansible-vim
       coc-nvim
@@ -55,7 +26,6 @@
       coc_filetype_map = {
         "yaml.ansible" = "ansible";
       };
-      # coc_global_extensions = [ "@yaegassy/coc-ansible" ];
     };
     plugins = {
       lualine.enable = true;
@@ -90,6 +60,8 @@
           nixd = {
             enable = true;
           };
+
+          # yaml
           yamlls = {
             enable = true;
           };
