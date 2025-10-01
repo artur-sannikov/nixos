e51304c798aa1a10cb463aaf22b687c67e1b9470
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-10-01 15:57:14 +0300

    feat: add defaultOptions to fzf

--- modules/home-manager/cli/fzf.nix
+++ modules/home-manager/cli/fzf.nix
@@ -2,6 +2,15 @@
   programs = {
     fzf = {
       enable = true;
+      defaultOptions = [
+        "--multi"
+        "--height=50%"
+        "--margin=5%,2%,2%,5%"
+        "--layout=reverse-list"
+        "--border=double"
+        "--prompt='$>''"
+        "--pointer='→'"
+      ];
       enableBashIntegration = true;
       enableZshIntegration = true;
       defaultCommand = "fd --hidden --strip-cwd-prefix --exclude .git";
