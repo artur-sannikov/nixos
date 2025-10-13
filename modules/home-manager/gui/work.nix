f0e44fe3cb444362f1f99fbf8ee06f6f240743b1
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-10-13 10:12:17 +0300

    feat: add seafile-client

--- modules/home-manager/gui/work.nix
+++ modules/home-manager/gui/work.nix
@@ -1,14 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-stable, ... }:
 {
   imports = [
     ./common.nix
   ];
   # Work apps
-  home.packages = with pkgs; [
-    slack
-    teams-for-linux
-    eduvpn-client
-    quickemu
-    #    seafile-client
-  ];
+  home.packages =
+    (with pkgs; [
+      slack
+      teams-for-linux
+      eduvpn-client
+      quickemu
+    ])
+    ++ (with pkgs-stable; [
+      seafile-client
+    ]);
 }
