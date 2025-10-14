41bf48ca2a2b25e5ef79925cd273aaefb10980fa
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-10-14 14:06:27 +0300

    feat: cache gpg-agent for 8 hours

--- modules/home-manager/cli/gnupg.nix
+++ modules/home-manager/cli/gnupg.nix
@@ -3,6 +3,8 @@
   programs.gpg.enable = true;
   services.gpg-agent = {
     enable = true;
+    maxCacheTtl = 28800;
+    defaultCacheTtl = 28800;
     pinentry.package = pkgs.pinentry-qt;
   };
 }
