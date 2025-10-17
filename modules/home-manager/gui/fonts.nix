249107471230b4fb8e8de360eb0072efea49e449
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-10-17 08:34:44 -0400

    feat: add iosevka-term font

--- modules/home-manager/gui/fonts.nix
+++ modules/home-manager/gui/fonts.nix
@@ -3,6 +3,7 @@
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
     nerd-fonts.iosevka
+    nerd-fonts.iosevka-term
     nerd-fonts.sauce-code-pro
     font-awesome
     dejavu_fonts
