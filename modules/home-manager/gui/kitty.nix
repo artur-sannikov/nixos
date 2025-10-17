89a7af5007dea38dae8126e1ce1aa7bb867c81ee
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-10-17 08:55:37 -0400

    feat: set font and a few settings for kitty

--- modules/home-manager/gui/kitty.nix
+++ modules/home-manager/gui/kitty.nix
@@ -1,13 +1,21 @@
+{ lib, ... }:
+# mkForce is required because default settings conflict with custom
+# definitions
 {
   programs = {
     kitty = {
       enable = true;
-      # font = {
-      #   name = "Iosevka Nerd Font Mono";
-      # };
+      font = {
+        name = lib.mkForce "Iosevka Term Extended";
+        size = lib.mkForce 12;
+      };
       shellIntegration = {
         enableBashIntegration = true;
         enableZshIntegration = true;
+      };
+      settings = {
+        scrollback_lines = 10000;
+        mouse_hide_wait = "-3.0";
       };
     };
   };
