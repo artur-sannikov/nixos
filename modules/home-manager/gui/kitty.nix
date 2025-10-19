bc0f33a0ab3bc53b40f5c69122bee7aca2955b01
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-10-19 13:03:15 +0300

    chore: add customizations to kitty terminal

--- modules/home-manager/gui/kitty.nix
+++ modules/home-manager/gui/kitty.nix
@@ -15,7 +15,12 @@
       };
       settings = {
         scrollback_lines = 10000;
+        # Hide mouse pointer when typing
         mouse_hide_wait = "-3.0";
+        enable_audio_bell = "no";
+        cursor_shape = "beam";
+        cursor_beam_thickness = 1.5;
+        cursor_blink_interval = 0;
       };
     };
   };
