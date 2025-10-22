9c0a2198eb8cf97f9e72ebdfcb8c99fa1d016d2d
Artur Sannikov <git-sign@asannikov.com>
2025-10-22 13:30:44 +0300

    feat(yazi): add zsh shell wrapper

--- modules/home-manager/cli/yazi.nix
+++ modules/home-manager/cli/yazi.nix
@@ -3,5 +3,17 @@
     yazi = {
       enable = true;
     };
+    zsh = {
+      initContent = ''
+          function y() {
+          local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+          yazi "$@" --cwd-file="$tmp"
+          if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+            cd -- "$cwd"
+          fi
+          rm -f -- "$tmp"
+        }
+      '';
+    };
   };
 }
