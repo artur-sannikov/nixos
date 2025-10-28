ff7d07348be2171e6f65216b0c82fdcb304a45c8
Artur Sannikov <git-sign@asannikov.com>
2025-10-28 22:11:13 +0200

    feat(docker): explicitly set storageDriver (btrfs by default)

--- modules/system/virtualization/docker.nix
+++ modules/system/virtualization/docker.nix
@@ -1,7 +1,9 @@
+{ lib, ... }:
 {
   virtualisation.docker = {
     # Disable system-wide docker
     enable = false;
+    storageDriver = lib.mkDefault "btrfs";
     rootless = {
       enable = true;
       setSocketVariable = true;
