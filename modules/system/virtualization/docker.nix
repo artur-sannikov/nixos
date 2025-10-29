fb2f1e515bb14fa0e04af6cd372bb6be32226262
Artur Sannikov <git-sign@asannikov.com>
2025-10-29 11:03:23 +0200

    feat(docker): enable autoPrune

--- modules/system/virtualization/docker.nix
+++ modules/system/virtualization/docker.nix
@@ -3,6 +3,10 @@
   virtualisation.docker = {
     # Disable system-wide docker
     enable = false;
+    autoPrune = {
+      enable = true;
+      dates = "13:00";
+    };
     storageDriver = lib.mkDefault "btrfs";
     rootless = {
       enable = true;
