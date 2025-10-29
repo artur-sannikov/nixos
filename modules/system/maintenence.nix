e2195a35cd89f2fe5e32f1995638a3a98ef96ebf
Artur Sannikov <git-sign@asannikov.com>
2025-10-29 11:00:28 +0200

    refactor: improve nix store maintenance
    Disable system auto-upgrade (managed via flake.lock).
    
    Disable store optimisation at every rebuild.
    
    Enable more frequent garbage collection.

--- modules/system/maintenence.nix
+++ modules/system/maintenence.nix
@@ -1,25 +1,24 @@
-{ flake-inputs, ... }:
 {
-  system.autoUpgrade = {
-    enable = true;
-    flake = flake-inputs.self.outPath;
-    flags = [
-      "nixpkgs"
-      "--commit-lock-file"
-      "-L" # print build logs
-    ];
-    dates = "18:00";
-    randomizedDelaySec = "45min";
+  system = {
+    autoUpgrade = {
+      # Managed via flake.lock
+      enable = false;
+    };
   };
   nix = {
-    settings.auto-optimise-store = true;
+    settings = {
+      # Runs at rebuild, slow
+      auto-optimise-store = false;
+    };
+    # Hard link identical files in nix store
     optimise = {
       automatic = true;
-      dates = [ "weekly" ];
+      dates = [ "15:00" ];
     };
     gc = {
       automatic = true;
-      options = "-d --delete-older-than 14d";
+      dates = [ "9:00" ];
+      options = "--delete-older-than 14d";
     };
   };
 }
