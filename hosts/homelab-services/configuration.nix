8a69f9893313c71e760d94f8afbb8c58bc9f7e39
Artur Sannikov <git-sign@asannikov.com>
2025-11-02 11:34:04 +0200

    feat: add forgjeo-dump NFS share

--- hosts/homelab-services/configuration.nix
+++ hosts/homelab-services/configuration.nix
@@ -57,6 +57,16 @@ in
         "_netdev"
       ];
     };
+    "/mnt/nas/backups/forgejo-dump" = {
+      device = "192.168.20.5:/mnt/tank/personal/backups/forgejo-dump";
+      fsType = "nfs";
+      options = [
+        "x-systemd.automount"
+        "nofail"
+        "noauto"
+        "_netdev"
+      ];
+    };
   };
 
   # Enable Flakes
