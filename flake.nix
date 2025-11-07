9f5aa8f4adb4fb5d8df599f115bc6661a200c576
Artur Sannikov <git-sign@asannikov.com>
2025-11-07 08:15:50 +0200

    chore: pin lanzaboote to v0.4.3

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lanzaboote = {
-      url = "github:nix-community/lanzaboote";
+      url = "github:nix-community/lanzaboote/v0.4.3";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # Remap keyboard keys
