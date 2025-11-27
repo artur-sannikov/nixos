bf8c16e43ed65794727c11cb3bcf32d892c6fe46
Artur Sannikov <git-sign@asannikov.com>
2025-11-27 21:27:30 +0200

    chore: remove backup mount from tuxedo

--- hosts/tuxedo/configuration.nix
+++ hosts/tuxedo/configuration.nix
@@ -139,16 +139,6 @@
 
     # Mount NFS
     fileSystems = {
-      # "/mnt/nas/backup" = {
-      #   device = "192.168.20.5:/mnt/tank/ux5401-backup";
-      #   fsType = "nfs";
-      #   options = [
-      #     "x-systemd.automount"
-      #     "nofail"
-      #     "noauto"
-      #     "_netdev"
-      #   ];
-      # };
       "/mnt/nas/media" = {
         device = "192.168.20.5:/mnt/tank/media";
         fsType = "nfs";
