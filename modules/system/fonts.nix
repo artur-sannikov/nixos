6b5aac38afb89306593caeb45c654c3bd3a9af63
Artur Sannikov <git-sign@asannikov.com>
2025-12-07 10:17:18 +0200

    chore: add more system fonts

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -1,10 +1,23 @@
-{ pkgs, ... }:
+{ pkgs-stable, ... }:
 {
-  fonts.packages = with pkgs; [
-    nerd-fonts.iosevka
-    source-code-pro
-    open-sans
-    liberation_ttf
-    roboto
-  ];
+  fonts = {
+    fontconfig = {
+      useEmbeddedBitmaps = true;
+    };
+    enableDefaultPackages = true;
+    packages = with pkgs-stable; [
+      nerd-fonts.iosevka
+      nerd-fonts.iosevka-term
+      nerd-fonts.sauce-code-pro
+      font-awesome
+      dejavu_fonts
+      noto-fonts
+      noto-fonts-cjk-serif
+      noto-fonts-cjk-sans
+      source-code-pro
+      open-sans
+      liberation_ttf
+      roboto
+    ];
+  };
 }
