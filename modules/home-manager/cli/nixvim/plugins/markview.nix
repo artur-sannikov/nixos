d605599712438ac6d3f9843a3a5f0dc22d7ea510
Artur Sannikov <git-sign@asannikov.com>
2025-12-15 13:04:56 +0200

    feat(nixvim): add keymap to toggle Markview

--- modules/home-manager/cli/nixvim/plugins/markview.nix
+++ modules/home-manager/cli/nixvim/plugins/markview.nix
@@ -1,5 +1,15 @@
 {
   programs.nixvim.plugins.markview = {
     enable = true;
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>mv";
+        action = "<cmd>Markview Toggle<CR>";
+        options = {
+          desc = "Toggle Markdown Preview";
+        };
+      }
+    ];
   };
 }
