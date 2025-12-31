977434e1e10205aabf4827049227c5ad685c92ce
Artur Sannikov <git-sign@asannikov.com>
2025-12-31 12:32:35 +0100

    refactor: switch lact to service module

--- modules/system/services/lact.nix
+++ modules/system/services/lact.nix
@@ -1,6 +1,7 @@
-{ pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [ lact ];
-  systemd.packages = with pkgs; [ lact ];
-  systemd.services.lactd.wantedBy = [ "multi-user.target" ];
+  services = {
+    lact = {
+      enable = true;
+    };
+  };
 }
