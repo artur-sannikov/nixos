def2ffaa1a51d25986806ff9e6dc18b1691388e4
Artur Sannikov <git-sign@asannikov.com>
2026-01-17 09:43:20 +0200

    refactor: remove openrgb systemd

--- modules/system/services/openrgb.nix
+++ modules/system/services/openrgb.nix
@@ -12,29 +12,16 @@
         sha256 = "sha256-vdIA9i1ewcrfX5U7FkcRR+ISdH5uRi9fz9YU5IkPKJQ=";
       };
       # See patch discussion https://github.com/NixOS/nixpkgs/issues/446002
-      patches = [
-        ./remove_systemd_service.patch
-      ];
-      postPatch = ''
-        patchShebangs scripts/build-udev-rules.sh
-        substituteInPlace scripts/build-udev-rules.sh \
-        --replace-fail /usr/bin/env "${pkgs.coreutils}/bin/env"
-      '';
+      # patches = [
+      #   ./remove_systemd_service.patch
+      # ];
+      # postPatch = ''
+      #   patchShebangs scripts/build-udev-rules.sh
+      #   substituteInPlace scripts/build-udev-rules.sh \
+      #   --replace-fail /usr/bin/env "${pkgs.coreutils}/bin/env"
+      # '';
       version = "1.0rc2";
     });
     motherboard = "amd";
-  };
-  systemd.services.openrgb = lib.mkDefault {
-    wantedBy = [ "multi-user.target" ];
-    after = [
-      "network.target"
-      "lm_sensors.service"
-    ];
-    description = "OpenRGB SDK Server";
-    serviceConfig = {
-      RemainAfterExit = "yes";
-      ExecStart = ''${pkgs.openrgb}/bin/openrgb --server'';
-      Restart = "always";
-    };
   };
 }
