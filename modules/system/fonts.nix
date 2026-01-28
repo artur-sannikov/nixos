4c21cdc6feaab207c0847047684f4e19778bca9a
Artur Sannikov <git-sign@asannikov.com>
2026-01-28 11:45:37 +0200

    fix: fonts

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -19,20 +19,20 @@
       };
     };
     enableDefaultPackages = true;
-    fonts.packages = with pkgs-stable; [
-      nerd-fonts.iosevka
-      nerd-fonts.iosevka-term
-      nerd-fonts.sauce-code-pro
-      font-awesome
-      dejavu_fonts
-      noto-fonts
-      noto-fonts-color-emoji
-      noto-fonts-cjk-serif
-      noto-fonts-cjk-sans
-      source-code-pro
-      open-sans
-      liberation_ttf
-      roboto
-    ];
   };
+  fonts.packages = with pkgs-stable; [
+    nerd-fonts.iosevka
+    nerd-fonts.iosevka-term
+    nerd-fonts.sauce-code-pro
+    font-awesome
+    dejavu_fonts
+    noto-fonts
+    noto-fonts-color-emoji
+    noto-fonts-cjk-serif
+    noto-fonts-cjk-sans
+    source-code-pro
+    open-sans
+    liberation_ttf
+    roboto
+  ];
 }
