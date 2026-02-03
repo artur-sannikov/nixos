ec60fb239707b40ab3ac3c8dcb33466815b0c089
Artur Sannikov <git-sign@asannikov.com>
2026-02-03 10:58:52 +0200

    feat: add custom settings and a keybind for gh-dash

--- modules/home-manager/cli/gh-dash.nix
+++ modules/home-manager/cli/gh-dash.nix
@@ -2,6 +2,34 @@
   programs = {
     gh-dash = {
       enable = true;
+      settings = {
+        prSections = [
+          {
+            title = "My Pull Requests";
+            filters = "is:open author:@me";
+            layout.author.hidden = true;
+          }
+          {
+            title = "Needs My Review";
+            filters = "is:open review-requested:@me";
+          }
+          {
+            title = "Involved";
+            filters = "is:open involves:@me -author:@me";
+          }
+        ];
+        keybindings = {
+          prs = [
+            {
+              key = "O";
+              name = "edit PR";
+              command = ''
+                nvim -c ":Octo pr edit {{.PrNumber}}"
+              '';
+            }
+          ];
+        };
+      };
     };
   };
 }
