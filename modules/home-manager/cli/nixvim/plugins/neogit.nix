76ce521ffc361a8a294509c244a6bf89edb3c167
Artur Sannikov <git-sign@asannikov.com>
2026-02-08 16:12:56 +0200

    feat(nixvim): show lines for Neogit and keymap to start Neogit

--- modules/home-manager/cli/nixvim/plugins/neogit.nix
+++ modules/home-manager/cli/nixvim/plugins/neogit.nix
@@ -4,9 +4,22 @@
       plugins = {
         neogit = {
           enable = true;
+          settings = {
+            disable_line_numbers = false;
+            disable_relative_line_numbers = false;
+          };
         };
       };
       keymaps = [
+        {
+          action = "<cmd>Neogit<CR>";
+          key = "<leader>ng";
+          mode = "n";
+          options = {
+            silent = true;
+            desc = "Show Neogit UI";
+          };
+        }
         {
           action = "<cmd>Neogit commit<CR>";
           key = "<leader>gc";
