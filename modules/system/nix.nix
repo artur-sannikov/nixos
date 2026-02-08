d3a42a22003cd5ce10f8503bce2dbd8d9884146a
Artur Sannikov <git-sign@asannikov.com>
2026-02-08 10:15:01 +0200

    feat: add nix-community binary cache

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -4,9 +4,11 @@
       # List of binary cache URLs that non-root users can use
       trusted-substituters = [
         "https://rstats-on-nix.cachix.org/"
+        "https://nix-community.cachix.org/"
       ];
       trusted-public-keys = [
         "rstats-on-nix.cachix.org-1:vdiiVgocg6WeJrODIqdprZRUrhi1JzhBnXv7aWI6+F0="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       trusted-users = [
         "root"
