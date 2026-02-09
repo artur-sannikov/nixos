885d417f1b7298af7f8933a1600dc005a321787e
Artur Sannikov <git-sign@asannikov.com>
2026-02-09 22:42:19 +0200

    feat(nixvim): keybinding to show all issues in repo

--- modules/home-manager/cli/nixvim/plugins/octo.nix
+++ modules/home-manager/cli/nixvim/plugins/octo.nix
@@ -6,6 +6,17 @@
           enable = true;
         };
       };
+      keymaps = [
+        {
+          mode = "n";
+          key = "<leader>oi";
+          action = "<cmd>Octo issue list<cr>";
+          options = {
+            silent = true;
+            desc = "List GitHub Issues";
+          };
+        }
+      ];
     };
   };
 }
