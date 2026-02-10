95ce1069b8f6c24bd983cfabeed0da3d5722a2b3
Artur Sannikov <git-sign@asannikov.com>
2026-02-10 10:46:39 +0200

    feat: set layouts in kitten

--- modules/home-manager/gui/kitty.nix
+++ modules/home-manager/gui/kitty.nix
@@ -24,6 +24,7 @@
         shell = "zsh";
         copy_on_select = "yes";
         tab_title_template = "{fmt.fg.red}{bell_symbol}{activity_symbol}{fmt.fg.tab}{tab.last_focused_progress_percent}{index}:{title}";
+        enabled_layouts = "stack,horizontal,vertical,splits,tall:bias=50;full_size=1;mirrored=true";
       };
       keybindings = {
         "ctrl+f>c" = "new_tab";
