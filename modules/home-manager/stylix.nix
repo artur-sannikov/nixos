c3e549a8139881cd233709054cd03618b0261562
Artur Sannikov <git-sign@asannikov.com>
2026-02-10 11:50:53 +0200

    fix: kitty colors
    Stylix configuration is a bit broken. For example, `radian` R console is
    barely visible.

--- modules/home-manager/stylix.nix
+++ modules/home-manager/stylix.nix
@@ -2,6 +2,10 @@
   stylix = {
     targets = {
       nixvim.enable = false;
+      kitty = {
+        # Kitty's colors are broken
+        enable = false;
+      };
       qt = {
         platform = "qtct";
       };
