1f1c8d1c614a33f1932d738782696feca694463b
Artur Sannikov <git-sign@asannikov.com>
2026-02-11 22:47:31 +0200

    feat: enable firewall for hetzner1

--- hosts/hetzner1/configuration.nix
+++ hosts/hetzner1/configuration.nix
@@ -22,6 +22,12 @@
 
   networking = {
     hostName = "hetzner1";
+    nftables = {
+      enable = true;
+    };
+    firewall = {
+      enable = true;
+    };
   };
 
   environment.systemPackages = with pkgs; [
