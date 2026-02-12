9eb5d6b00536b86cdce0007d21a37c32b7459fd4
Artur Sannikov <git-sign@asannikov.com>
2026-02-12 10:14:56 +0200

    feat(nixvim): add octo.nvim keymaps

--- modules/home-manager/cli/nixvim/plugins/octo.nix
+++ modules/home-manager/cli/nixvim/plugins/octo.nix
@@ -16,6 +16,46 @@
             desc = "List GitHub Issues";
           };
         }
+        {
+          mode = "n";
+          key = "<leader>op";
+          action = "<cmd>Octo pr list<cr>";
+          options = {
+            silent = true;
+            desc = "List GitHub PRs";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>on";
+          action = "<cmd>Octo notification list<cr>";
+          options = {
+            silent = true;
+            desc = "List GitHub Notifications";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>od";
+          action = "<cmd>Octo discussion list<cr>";
+          options = {
+            silent = true;
+            desc = "List GitHub Discussions";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>os";
+          action.__raw = ''
+            function()
+              require("octo.utils").create_base_search_command { include_current_repo = true }
+            end
+          '';
+          options = {
+            silent = true;
+            desc = "Search GitHub";
+          };
+        }
       ];
     };
   };
