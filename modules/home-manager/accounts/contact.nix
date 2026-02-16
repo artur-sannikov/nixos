362079853c2784dc1d0245708784c4e485f92bd2
Artur Sannikov <git-sign@asannikov.com>
2026-02-16 14:55:26 +0200

    fix: contacts sync

--- modules/home-manager/accounts/contact.nix
+++ modules/home-manager/accounts/contact.nix
@@ -6,7 +6,7 @@
   ...
 }:
 let
-  carddav_url = "https://nextcloud.${flake-inputs.nix-secrets.domain}/remote.php/dav/addressbooks/users/${username}/default/";
+  carddav_url = "https://nextcloud.${flake-inputs.nix-secrets.domain}/remote.php/dav/addressbooks/users/${username}";
 in
 {
   accounts = {
@@ -16,13 +16,22 @@ in
         default = {
           khard = {
             enable = true;
+            type = "discover"; # Allow multiple address books
           };
           pimsync = {
             enable = true;
+            extraPairDirectives = [
+              {
+                name = "collection";
+                params = [
+                  "work-1"
+                ];
+              }
+            ];
           };
           local = {
             type = "filesystem";
-            fileExt = ".vcf";
+            fileExt = "vcf";
           };
           remote = {
             passwordCommand = [
@@ -30,7 +39,7 @@ in
               config.sops.secrets."pimsync/nextcloud/password".path
             ];
             type = "carddav";
-            url = carddav_url;
+            url = "${carddav_url}/work-1";
             userName = "${username}";
           };
         };
