1ad7f49e3eb943abfc22924dafde5934bac56523
Artur Sannikov <git-sign@asannikov.com>
2026-02-16 10:36:43 +0200

    feat: set up brave

--- modules/home-manager/gui/browsers/brave.nix
+++ modules/home-manager/gui/browsers/brave.nix
@@ -3,10 +3,6 @@
   programs.chromium = {
     enable = true;
     package = pkgs.brave;
-    extensions = [
-      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin
-      { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
-    ];
     commandLineArgs = [
       "--enable-features=UseOzonePlatform"
       "--ozone-platform=wayland"
