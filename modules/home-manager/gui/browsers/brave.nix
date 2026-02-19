93e20b6d7e3eec291942a20abd5be7fc501f5aff
Artur Sannikov <git-sign@asannikov.com>
2026-02-19 17:58:05 +0200

    chore: styling

--- modules/home-manager/gui/browsers/brave.nix
+++ modules/home-manager/gui/browsers/brave.nix
@@ -1,12 +1,14 @@
 { pkgs, ... }:
 {
-  programs.chromium = {
-    enable = true;
-    package = pkgs.brave;
-    commandLineArgs = [
-      "--enable-features=UseOzonePlatform"
-      "--ozone-platform=wayland"
-      "--no-default-browser-check"
-    ];
+  programs = {
+    chromium = {
+      enable = true;
+      package = pkgs.brave;
+      commandLineArgs = [
+        "--enable-features=UseOzonePlatform"
+        "--ozone-platform=wayland"
+        "--no-default-browser-check"
+      ];
+    };
   };
 }
--- modules/system/browsers/brave.nix
+++ modules/system/browsers/brave.nix
@@ -1,77 +1,79 @@
-{ pkgs, ... }:
 {
-  programs.chromium = {
-    enable = true;
-    extensions = [
-      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
-      "nngceckbapebfimnlniiiahkandclblb" # bitwarden
-    ];
-    enablePlasmaBrowserIntegration = true;
-    defaultSearchProviderEnabled = true;
-    defaultSearchProviderSearchURL = "https://noai.duckduckgo.com/?q=";
-    extraOpts = {
-      # Search options
-      "DefaultSearchProviderName" = "DuckDuckGo";
-      "DefaultSearchProviderKeyword" = "ddg";
-      # Content Settings
-      "DefaultGeolocationSetting" = 2;
-      "DefaultNotificationsSetting" = 2;
-      "DefaultLocalFontsSetting" = 2;
-      "DefaultSensorsSetting" = 2;
-      "DefaultSerialGuardSetting" = 2;
-      "SyncDisabled" = true;
-      "PasswordManagerEnabled" = false;
-      "PasswordSharingEnabled" = false;
-      "PasswordLeakDetectionEnabled" = false;
-      "SafeBrowsingExtendedReportingEnabled" = false;
-      "SafeBrowsingSurveysEnabled" = false;
-      # User and device reporting
-      "DeviceActivityHeartbeatEnabled" = false;
-      "DeviceMetricsReportingEnabled" = false;
-      "HeartbeatEnabled" = false;
-      "LogUploadEnabled" = false;
-      "ReportAppInventory" = "";
-      "ReportDeviceActivityTimes" = false;
-      "ReportDeviceAppInfo" = false;
-      "ReportDeviceSystemInfo" = false;
-      "ReportDeviceUsers" = false;
-      "ReportWebsiteTelemetry" = "";
-      # Quick Answers
-      "QuickAnswersEnabled" = false;
-      # Miscellaneous
-      "AlternateErrorPagesEnabled" = false;
-      "AutofillCreditCardEnabled" = false;
-      "BackgroundModeEnabled" = false;
-      "BrowserGuestModeEnabled" = false;
-      "BrowserSignin" = 0;
-      "BuiltInDnsClientEnabled" = false;
-      "DefaultBrowserSettingEnabled" = false;
-      "MetricsReportingEnabled" = false;
-      "ParcelTrackingEnabled" = false;
-      "RelatedWebsiteSetsEnabled" = false;
-      "ShoppingListEnabled" = false;
-      "WarnBeforeQuittingEnabled" = true;
-      "AlwaysOpenPdfExternally" = true;
-      "SpellcheckLanguage" = [
-        "fi"
-        "it"
-        "en-US"
+  programs = {
+    chromium = {
+      enable = true;
+      extensions = [
+        "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
+        "nngceckbapebfimnlniiiahkandclblb" # bitwarden
+        "dbepggeogbaibhgnhhndojpepiihcmeb" # vimium
       ];
-      # Brave stuff
-      "BraveAIChatEnabled" = false;
-      "BraveNewsDisabled" = true;
-      "BraveP3AEnabled" = false;
-      "BraveRewardsDisabled" = true;
-      "BraveSpeedreaderEnabled" = false;
-      "BraveStatsPingEnabled" = false;
-      "BraveSyncUrl" = "";
-      "BraveTalkDisabled" = true;
-      "BraveVPNDisabled" = true;
-      "BraveWalletDisabled" = true;
-      "BraveWaybackMachineEnabled" = false;
-      "BraveWebDiscoveryEnabled" = false;
-      "TorDisabled" = true;
-      "BravePlaylistEnabled" = false;
+      enablePlasmaBrowserIntegration = true;
+      defaultSearchProviderEnabled = true;
+      defaultSearchProviderSearchURL = "https://noai.duckduckgo.com/?q=";
+      extraOpts = {
+        # Search options
+        "DefaultSearchProviderName" = "DuckDuckGo";
+        "DefaultSearchProviderKeyword" = "ddg";
+        # Content Settings
+        "DefaultGeolocationSetting" = 2;
+        "DefaultNotificationsSetting" = 2;
+        "DefaultLocalFontsSetting" = 2;
+        "DefaultSensorsSetting" = 2;
+        "DefaultSerialGuardSetting" = 2;
+        "SyncDisabled" = true;
+        "PasswordManagerEnabled" = false;
+        "PasswordSharingEnabled" = false;
+        "PasswordLeakDetectionEnabled" = false;
+        "SafeBrowsingExtendedReportingEnabled" = false;
+        "SafeBrowsingSurveysEnabled" = false;
+        # User and device reporting
+        "DeviceActivityHeartbeatEnabled" = false;
+        "DeviceMetricsReportingEnabled" = false;
+        "HeartbeatEnabled" = false;
+        "LogUploadEnabled" = false;
+        "ReportAppInventory" = "";
+        "ReportDeviceActivityTimes" = false;
+        "ReportDeviceAppInfo" = false;
+        "ReportDeviceSystemInfo" = false;
+        "ReportDeviceUsers" = false;
+        "ReportWebsiteTelemetry" = "";
+        # Quick Answers
+        "QuickAnswersEnabled" = false;
+        # Miscellaneous
+        "AlternateErrorPagesEnabled" = false;
+        "AutofillCreditCardEnabled" = false;
+        "BackgroundModeEnabled" = false;
+        "BrowserGuestModeEnabled" = false;
+        "BrowserSignin" = 0;
+        "BuiltInDnsClientEnabled" = false;
+        "DefaultBrowserSettingEnabled" = false;
+        "MetricsReportingEnabled" = false;
+        "ParcelTrackingEnabled" = false;
+        "RelatedWebsiteSetsEnabled" = false;
+        "ShoppingListEnabled" = false;
+        "WarnBeforeQuittingEnabled" = true;
+        "AlwaysOpenPdfExternally" = true;
+        "SpellcheckLanguage" = [
+          "fi"
+          "it"
+          "en-US"
+        ];
+        # Brave stuff
+        "BraveAIChatEnabled" = false;
+        "BraveNewsDisabled" = true;
+        "BraveP3AEnabled" = false;
+        "BraveRewardsDisabled" = true;
+        "BraveSpeedreaderEnabled" = false;
+        "BraveStatsPingEnabled" = false;
+        "BraveSyncUrl" = "";
+        "BraveTalkDisabled" = true;
+        "BraveVPNDisabled" = true;
+        "BraveWalletDisabled" = true;
+        "BraveWaybackMachineEnabled" = false;
+        "BraveWebDiscoveryEnabled" = false;
+        "TorDisabled" = true;
+        "BravePlaylistEnabled" = false;
+      };
     };
   };
 }
