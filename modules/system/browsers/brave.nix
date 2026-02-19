6112910af5f497835d60b3e97ecc5c7900732c76
Artur Sannikov <git-sign@asannikov.com>
2026-02-19 18:23:53 +0200

    refactor(brave): reorganize policies

--- modules/system/browsers/brave.nix
+++ modules/system/browsers/brave.nix
@@ -9,69 +9,78 @@
       ];
       defaultSearchProviderEnabled = true;
       defaultSearchProviderSearchURL = "https://noai.duckduckgo.com/?q={searchTerms}";
+      # https://chromeenterprise.google/policies/
       extraOpts = {
         # Search options
-        "DefaultSearchProviderName" = "DuckDuckGo";
-        "DefaultSearchProviderKeyword" = "ddg";
+        DefaultSearchProviderName = "DuckDuckGo";
+        DefaultSearchProviderKeyword = "ddg";
         # Content Settings
-        "DefaultGeolocationSetting" = 2;
-        "DefaultNotificationsSetting" = 2;
-        "DefaultLocalFontsSetting" = 2;
-        "DefaultSensorsSetting" = 2;
-        "DefaultSerialGuardSetting" = 2;
-        "SyncDisabled" = true;
-        "PasswordManagerEnabled" = false;
-        "PasswordSharingEnabled" = false;
-        "PasswordLeakDetectionEnabled" = false;
-        "SafeBrowsingExtendedReportingEnabled" = false;
-        "SafeBrowsingSurveysEnabled" = false;
+        DefaultGeolocationSetting = 2;
+        DefaultNotificationsSetting = 2;
+        DefaultLocalFontsSetting = 2;
+        DefaultSensorsSetting = 2;
+        DefaultSerialGuardSetting = 2;
+        SyncDisabled = true;
+        PasswordManagerEnabled = false;
+        PasswordSharingEnabled = false;
+        PasswordLeakDetectionEnabled = false;
+        SafeBrowsingExtendedReportingEnabled = false;
+        SafeBrowsingSurveysEnabled = false;
         # User and device reporting
-        "DeviceActivityHeartbeatEnabled" = false;
-        "DeviceMetricsReportingEnabled" = false;
-        "HeartbeatEnabled" = false;
-        "LogUploadEnabled" = false;
-        "ReportAppInventory" = "";
-        "ReportDeviceActivityTimes" = false;
-        "ReportDeviceAppInfo" = false;
-        "ReportDeviceSystemInfo" = false;
-        "ReportDeviceUsers" = false;
-        "ReportWebsiteTelemetry" = "";
+        DeviceActivityHeartbeatEnabled = false;
+        DeviceMetricsReportingEnabled = false;
+        HeartbeatEnabled = false;
+        LogUploadEnabled = false;
+        ReportAppInventory = "";
+        ReportDeviceActivityTimes = false;
+        ReportDeviceAppInfo = false;
+        ReportDeviceSystemInfo = false;
+        ReportDeviceUsers = false;
+        ReportWebsiteTelemetry = "";
         # Quick Answers
-        "QuickAnswersEnabled" = false;
+        QuickAnswersEnabled = false;
         # Miscellaneous
-        "AlternateErrorPagesEnabled" = false;
-        "AutofillCreditCardEnabled" = false;
-        "BackgroundModeEnabled" = false;
-        "BrowserGuestModeEnabled" = false;
-        "BrowserSignin" = 0;
-        "BuiltInDnsClientEnabled" = false;
-        "DefaultBrowserSettingEnabled" = false;
-        "MetricsReportingEnabled" = false;
-        "ParcelTrackingEnabled" = false;
-        "RelatedWebsiteSetsEnabled" = false;
-        "ShoppingListEnabled" = false;
-        "WarnBeforeQuittingEnabled" = true;
-        "AlwaysOpenPdfExternally" = true;
-        "SpellcheckLanguage" = [
+        AlternateErrorPagesEnabled = false;
+        AutofillCreditCardEnabled = false;
+        BackgroundModeEnabled = false;
+        BrowserGuestModeEnabled = false;
+        BrowserSignin = 0;
+        BuiltInDnsClientEnabled = false;
+        DefaultBrowserSettingEnabled = false;
+        MetricsReportingEnabled = false;
+        ParcelTrackingEnabled = false;
+        RelatedWebsiteSetsEnabled = false;
+        ShoppingListEnabled = false;
+        WarnBeforeQuittingEnabled = true;
+        AlwaysOpenPdfExternally = true;
+        AllowLanguages = [
+          "fi"
+          "en-US"
+        ];
+        ForceLanguages = [
+          "fi"
+          "en-US"
+        ];
+        SpellcheckLanguage = [
           "fi"
           "it"
           "en-US"
         ];
         # Brave stuff
-        "BraveAIChatEnabled" = false;
-        "BraveNewsDisabled" = true;
-        "BraveP3AEnabled" = false;
-        "BraveRewardsDisabled" = true;
-        "BraveSpeedreaderEnabled" = false;
-        "BraveStatsPingEnabled" = false;
-        "BraveSyncUrl" = "";
-        "BraveTalkDisabled" = true;
-        "BraveVPNDisabled" = true;
-        "BraveWalletDisabled" = true;
-        "BraveWaybackMachineEnabled" = false;
-        "BraveWebDiscoveryEnabled" = false;
-        "TorDisabled" = true;
-        "BravePlaylistEnabled" = false;
+        BraveAIChatEnabled = false;
+        BraveNewsDisabled = true;
+        BraveP3AEnabled = false;
+        BraveRewardsDisabled = true;
+        BraveSpeedreaderEnabled = false;
+        BraveStatsPingEnabled = false;
+        BraveSyncUrl = "";
+        BraveTalkDisabled = true;
+        BraveVPNDisabled = true;
+        BraveWalletDisabled = true;
+        BraveWaybackMachineEnabled = false;
+        BraveWebDiscoveryEnabled = false;
+        TorDisabled = true;
+        BravePlaylistEnabled = false;
       };
     };
   };
