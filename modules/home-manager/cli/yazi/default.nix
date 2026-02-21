3a546c87fe663748f52bf6dd30b3024ba2e7f463
Artur Sannikov <git-sign@asannikov.com>
2026-02-21 12:04:26 +0200

    refactor(yazi): add native zsh and bash integrations

--- modules/home-manager/cli/yazi/default.nix
+++ modules/home-manager/cli/yazi/default.nix
@@ -2,18 +2,11 @@
   programs = {
     yazi = {
       enable = true;
-    };
-    zsh = {
-      initContent = ''
-          function y() {
-          local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-          yazi "$@" --cwd-file="$tmp"
-          if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-            cd -- "$cwd"
-          fi
-          rm -f -- "$tmp"
-        }
-      '';
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+
+      # New behaviour
+      shellWrapperName = "y";
     };
   };
 }
