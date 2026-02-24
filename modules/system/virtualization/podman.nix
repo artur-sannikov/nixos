399af854afd6e1b29980de47155fccf3e4ad5308
Artur Sannikov <git-sign@asannikov.com>
2026-02-24 21:39:32 +0200

    feat(podman): add autoPrune

--- modules/system/virtualization/podman.nix
+++ modules/system/virtualization/podman.nix
@@ -1,15 +1,25 @@
 {
   virtualisation = {
-    containers.storage.settings = {
-      # defaults
-      storage = {
-        driver = "overlay";
-        graphroot = "/var/lib/containers/storage";
-        runroot = "/run/containers/storage";
-      };
-    };
     podman = {
       enable = true;
+      autoPrune = {
+        enable = true;
+        flags = [
+          "--all"
+        ];
+      };
+    };
+    containers = {
+      storage = {
+        settings = {
+          # defaults
+          storage = {
+            driver = "overlay";
+            graphroot = "/var/lib/containers/storage";
+            runroot = "/run/containers/storage";
+          };
+        };
+      };
     };
   };
 }
