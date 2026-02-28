29fec97dc54297dd2c0c29ed11d2251c0b899975
Artur Sannikov <git-sign@asannikov.com>
2026-02-28 17:10:01 +0200

    feat(nix): add netrc for nix configuration
    Also manually deleted `~/.config/nix/nix.conf` because it was
    conflicting with system-wide nix configuration.

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -1,5 +1,10 @@
+{ config, ... }:
 {
-
+  sops = {
+    secrets = {
+      "atticd/netrc" = { };
+    };
+  };
   nix = {
     settings = {
       # List of binary cache URLs that non-root users can use
@@ -7,13 +12,13 @@
         "https://nix-community.cachix.org?priority=10"
         "https://numtide.cachix.org?priority=11"
         "https://rstats-on-nix.cachix.org?priority=12"
-        "https://cache.asannikov.com/otter?priority=13"
+        "https://cache.asannikov.com/main?priority=13"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
         "rstats-on-nix.cachix.org-1:vdiiVgocg6WeJrODIqdprZRUrhi1JzhBnXv7aWI6+F0="
-        "otter:qry3jdoFrVZ1PwPUhJaZix3S2LBLE6fRMXoxk/yNvvQ="
+        "main:qry3jdoFrVZ1PwPUhJaZix3S2LBLE6fRMXoxk/yNvvQ="
       ];
       trusted-users = [
         "root"
@@ -23,6 +28,7 @@
         "nix-command"
         "flakes"
       ];
+      netrc-file = config.sops.secrets."atticd/netrc".path;
     };
   };
 }
