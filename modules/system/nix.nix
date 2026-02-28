be9c5228ff1efa3a5430c61f9eac30eb6df15358
Artur Sannikov <git-sign@asannikov.com>
2026-02-28 13:52:47 +0200

    feat: add private binary cache

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -7,11 +7,13 @@
         "https://nix-community.cachix.org?priority=10"
         "https://numtide.cachix.org?priority=11"
         "https://rstats-on-nix.cachix.org?priority=12"
+        "https://cache.asannikov.com/otter?priority=13"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
         "rstats-on-nix.cachix.org-1:vdiiVgocg6WeJrODIqdprZRUrhi1JzhBnXv7aWI6+F0="
+        "otter:qry3jdoFrVZ1PwPUhJaZix3S2LBLE6fRMXoxk/yNvvQ="
       ];
       trusted-users = [
         "root"
