91c2d79bc13e40c1c20945544f0d616b6e6ab1ec
Artur Sannikov <git-sign@asannikov.com>
2026-03-02 22:29:19 +0200

    chore(nixvim): restructure yarepl

--- modules/home-manager/cli/nixvim/plugins/yarepl.nix
+++ modules/home-manager/cli/nixvim/plugins/yarepl.nix
@@ -1,45 +1,47 @@
 { pkgs, ... }:
 {
-  programs.nixvim = {
-    extraPlugins = [
-      (pkgs.vimUtils.buildVimPlugin {
-        name = "yarepl.nvim";
-        src = pkgs.fetchFromGitHub {
-          owner = "milanglacier";
-          repo = "yarepl.nvim";
-          rev = "v0.12.0";
-          hash = "sha256-3JkrQCTvWFE6CRRbeiqqmA8tVVl4ZzE+hQX75ODuHo8=";
-        };
-        nvimSkipModules = [ "yarepl.extensions.fzf" ];
-      })
-    ];
-    extraConfigLua = builtins.readFile ./yarepl.lua;
-    keymaps = [
-      {
-        mode = "n";
-        key = "<leader>rs";
-        action = "<Plug>(REPLStart)";
-      }
-      {
-        mode = "n";
-        key = "<leader>rc";
-        action = "<Plug>(REPLClose)";
-      }
-      {
-        mode = "n";
-        key = "<leader>rr";
-        action = "<Plug>(REPLSendLine)j";
-      }
-      {
-        mode = "v";
-        key = "<leader>rr";
-        action = "<Plug>(REPLSendVisual)";
-      }
-      {
-        mode = "n";
-        key = "<leader>rf";
-        action = "<Plug>(REPLFocus)";
-      }
-    ];
+  programs = {
+    nixvim = {
+      extraPlugins = [
+        (pkgs.vimUtils.buildVimPlugin {
+          name = "yarepl.nvim";
+          src = pkgs.fetchFromGitHub {
+            owner = "milanglacier";
+            repo = "yarepl.nvim";
+            rev = "v0.12.0";
+            hash = "sha256-3JkrQCTvWFE6CRRbeiqqmA8tVVl4ZzE+hQX75ODuHo8=";
+          };
+          nvimSkipModules = [ "yarepl.extensions.fzf" ];
+        })
+      ];
+      extraConfigLua = builtins.readFile ./yarepl.lua;
+      keymaps = [
+        {
+          mode = "n";
+          key = "<leader>rs";
+          action = "<Plug>(REPLStart)";
+        }
+        {
+          mode = "n";
+          key = "<leader>rc";
+          action = "<Plug>(REPLClose)";
+        }
+        {
+          mode = "n";
+          key = "<leader>rr";
+          action = "<Plug>(REPLSendLine)j";
+        }
+        {
+          mode = "v";
+          key = "<leader>rr";
+          action = "<Plug>(REPLSendVisual)";
+        }
+        {
+          mode = "n";
+          key = "<leader>rf";
+          action = "<Plug>(REPLFocus)";
+        }
+      ];
+    };
   };
 }
