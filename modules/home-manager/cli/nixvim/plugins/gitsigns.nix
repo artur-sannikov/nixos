820375c93fa36ba4c6aaca81557858b4810ffd0a
Artur Sannikov <git-sign@asannikov.com>
2026-03-09 13:32:37 +0200

    feat(nixvim): add keybindings for gitsigns

--- modules/home-manager/cli/nixvim/plugins/gitsigns.nix
+++ modules/home-manager/cli/nixvim/plugins/gitsigns.nix
@@ -4,4 +4,33 @@
       enable = true;
     };
   };
+  keymaps = [
+    {
+      mode = "n";
+      key = "[c";
+      action = "<cmd>Gitsigns next_hunk<cr>";
+      options = {
+        silent = true;
+        desc = "Next Git Hunk";
+      };
+    }
+    {
+      mode = "n";
+      key = "]c";
+      action = "<cmd>Gitsigns prev_hunk<cr>";
+      options = {
+        silent = true;
+        desc = "Previous Git Hunk";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>hd";
+      action = "<cmd>Gitsigns diffthis<cr>";
+      options = {
+        silent = true;
+        desc = "Show hunk diffs";
+      };
+    }
+  ];
 }
