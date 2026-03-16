9b076b95325537873cebdcb048a2673a7c430a96
Artur Sannikov <git-sign@asannikov.com>
2026-03-16 19:40:32 +0200

    refactor(syncoid): remove debug flag

--- modules/system/services/syncoid.nix
+++ modules/system/services/syncoid.nix
@@ -12,26 +12,27 @@
       };
     };
   };
-  services.syncoid = {
-    enable = true;
-    sshKey = config.sops.secrets."keys/ssh/syncoid-ssh-key".path;
-    commonArgs = [
-      "--no-sync-snap"
-      "--debug"
-    ];
-    commands = {
-      "home" = {
-        source = "zroot/home";
-        target = "syncoid@192.168.20.5:tank/personal/backups/${config.networking.hostName}";
-        extraArgs = [
-          "--exclude-datasets=Downloads"
-          "--exclude-datasets=zfs_exclude"
-          "--delete-target-snapshots" # Delete snapshots on target which are missing on source
-          "--sshoption=StrictHostKeyChecking=off"
-        ];
-        recursive = false;
-        sendOptions = "w"; # raw
-        recvOptions = "u"; # umount
+  services = {
+    syncoid = {
+      enable = true;
+      sshKey = config.sops.secrets."keys/ssh/syncoid-ssh-key".path;
+      commonArgs = [
+        "--no-sync-snap"
+      ];
+      commands = {
+        "home" = {
+          source = "zroot/home";
+          target = "syncoid@192.168.20.5:tank/personal/backups/${config.networking.hostName}";
+          extraArgs = [
+            "--exclude-datasets=Downloads"
+            "--exclude-datasets=zfs_exclude"
+            "--delete-target-snapshots" # Delete snapshots on target which are missing on source
+            "--sshoption=StrictHostKeyChecking=off"
+          ];
+          recursive = false;
+          sendOptions = "w"; # raw
+          recvOptions = "u"; # umount
+        };
       };
     };
   };
