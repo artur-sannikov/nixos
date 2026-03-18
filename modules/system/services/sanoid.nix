0d54ca57d7bd7a251aa1966b3b8abf65407030d6
Artur Sannikov <git-sign@asannikov.com>
2026-03-18 21:49:24 +0200

    refactor(sanoid): move sanoid package to its service

--- modules/system/services/sanoid.nix
+++ modules/system/services/sanoid.nix
@@ -1,34 +1,40 @@
+{ pkgs, ... }:
 {
-  services.sanoid = {
-    enable = true;
-    extraArgs = [
-      "--verbose"
-    ];
-    interval = "hourly";
-    templates = {
-      "backup" = {
-        "hourly" = 36;
-        "daily" = 30;
-        "monthly" = 3;
-        "yearly" = 0;
-        "autosnap" = true;
-        "autoprune" = true;
+  services = {
+    sanoid = {
+      enable = true;
+      extraArgs = [
+        "--verbose"
+      ];
+      interval = "hourly";
+      templates = {
+        "backup" = {
+          "hourly" = 36;
+          "daily" = 30;
+          "monthly" = 3;
+          "yearly" = 0;
+          "autosnap" = true;
+          "autoprune" = true;
+        };
+        "none" = {
+          "autosnap" = false;
+          "autoprune" = false;
+        };
       };
-      "none" = {
-        "autosnap" = false;
-        "autoprune" = false;
+      datasets = {
+        "zroot/home" = {
+          useTemplate = [ "backup" ];
+        };
+        "zroot/home/Downloads" = {
+          useTemplate = [ "none" ];
+        };
+        "zroot/home/zfs_exclude" = {
+          useTemplate = [ "none" ];
+        };
       };
     };
-    datasets = {
-      "zroot/home" = {
-        useTemplate = [ "backup" ];
-      };
-      "zroot/home/Downloads" = {
-        useTemplate = [ "none" ];
-      };
-      "zroot/home/zfs_exclude" = {
-        useTemplate = [ "none" ];
-      };
-    };
+  };
+  environment = {
+    systemPackages = with pkgs; [ sanoid ];
   };
 }
