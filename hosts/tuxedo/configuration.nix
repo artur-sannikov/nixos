35e746bb6e3d4890790019d2199e4ebeaf9697ca
Artur Sannikov <git-sign@asannikov.com>
2026-05-24 09:38:50 +0300

    refactor(tuxedo): set zfs.forceImportRoot to false

--- hosts/tuxedo/configuration.nix
+++ hosts/tuxedo/configuration.nix
@@ -91,6 +91,9 @@
 
     boot = {
       extraModulePackages = with config.boot.kernelPackages; [ yt6801 ];
+      zfs = {
+        forceImportRoot = false; # default from 26.11
+      };
       kernelParams = [
         "acpi.ec_no_wakeup=1" # Fixes ACPI wakeup issues
         "amdgpu.dcdebugmask=0x10" # Fixes Wayland slowdowns/freezes
