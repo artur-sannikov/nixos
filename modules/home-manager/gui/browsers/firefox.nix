29ab9a87a47a9ada7c18e418831eb87148c79799
Artur Sannikov <git-sign@asannikov.com>
2026-06-06 22:50:43 +0300

    chore(firefox): explicitly set configPath

--- modules/home-manager/gui/browsers/firefox.nix
+++ modules/home-manager/gui/browsers/firefox.nix
@@ -1,40 +1,44 @@
+{ config, ... }:
 {
-  programs.firefox = {
-    enable = true;
-    profiles = {
-      default-release = {
-        isDefault = true;
-        search = {
-          force = true;
-          default = "ddg";
-          privateDefault = "ddg";
+  programs = {
+    firefox = {
+      configPath = "${config.xdg.configHome}/mozilla/firefox";
+      enable = true;
+      profiles = {
+        default-release = {
+          isDefault = true;
+          search = {
+            force = true;
+            default = "ddg";
+            privateDefault = "ddg";
+          };
         };
-      };
-      tidal-hifi = {
-        isDefault = false;
-        id = 1;
-        bookmarks = {
-          force = true;
-          settings = [
-            {
-              name = "tidal";
-              url = "https://listen.tidal.com/";
-            }
-          ];
+        tidal-hifi = {
+          isDefault = false;
+          id = 1;
+          bookmarks = {
+            force = true;
+            settings = [
+              {
+                name = "tidal";
+                url = "https://listen.tidal.com/";
+              }
+            ];
+          };
+          search = {
+            force = true;
+            default = "ddg";
+            privateDefault = "ddg";
+          };
         };
-        search = {
-          force = true;
-          default = "ddg";
-          privateDefault = "ddg";
-        };
-      };
-      violentmonkey = {
-        isDefault = false;
-        id = 2;
-        search = {
-          force = true;
-          default = "ddg";
-          privateDefault = "ddg";
+        violentmonkey = {
+          isDefault = false;
+          id = 2;
+          search = {
+            force = true;
+            default = "ddg";
+            privateDefault = "ddg";
+          };
         };
       };
     };
