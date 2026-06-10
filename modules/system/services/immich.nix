c9b34d2b4697fcce04c6ff4e0fefa5b70194ea6b
Artur Sannikov <git-sign@asannikov.com>
2026-06-10 23:22:08 +0300

    feat(dendritic): move immich

--- modules/system/services/immich.nix
+++ modules/system/services/immich.nix
@@ -1,40 +1,42 @@
 {
-  # Mount NFS
-  fileSystems = {
-    "/mnt/nas/photos" = {
-      device = "192.168.20.5:/mnt/tank/personal/photos";
-      fsType = "nfs";
-      options = [
-        "x-systemd.automount"
-        "nofail"
-        "noauto"
-        "_netdev"
-      ];
+  flake.nixosModules.immich = {
+    # Mount NFS
+    fileSystems = {
+      "/mnt/nas/photos" = {
+        device = "192.168.20.5:/mnt/tank/personal/photos";
+        fsType = "nfs";
+        options = [
+          "x-systemd.automount"
+          "nofail"
+          "noauto"
+          "_netdev"
+        ];
+      };
     };
-  };
 
-  users = {
     users = {
-      immich = {
-        isSystemUser = true;
-        group = "immich";
+      users = {
+        immich = {
+          isSystemUser = true;
+          group = "immich";
+        };
+      };
+      groups = {
+        immich = {
+          gid = 1002;
+        };
       };
     };
-    groups = {
-      immich = {
-        gid = 1002;
-      };
-    };
-  };
 
-  services = {
-    immich = {
-      enable = true;
-      user = "immich";
-      group = "immich";
-      openFirewall = true;
-      host = "0.0.0.0";
-      mediaLocation = "/mnt/nas/photos";
+    services = {
+      immich = {
+        enable = true;
+        user = "immich";
+        group = "immich";
+        openFirewall = true;
+        host = "0.0.0.0";
+        mediaLocation = "/mnt/nas/photos";
+      };
     };
   };
 }
