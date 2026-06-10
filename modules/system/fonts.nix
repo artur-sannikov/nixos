31ec1400d7d7227159f8d789fdc0adabbb8d9355
Artur Sannikov <git-sign@asannikov.com>
2026-06-10 23:22:14 +0300

    feat(dendritic): move fonts

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -1,26 +1,28 @@
 { pkgs-stable, ... }:
 {
-  fonts = {
-    enableDefaultPackages = true;
-    fontconfig = {
-      useEmbeddedBitmaps = true;
+  flake.nixosModules.base = {
+    fonts = {
+      enableDefaultPackages = true;
+      fontconfig = {
+        useEmbeddedBitmaps = true;
+      };
+      packages = with pkgs-stable; [
+        nerd-fonts.iosevka
+        nerd-fonts.iosevka-term
+        nerd-fonts.ubuntu-sans
+        nerd-fonts.sauce-code-pro
+        nerd-fonts.jetbrains-mono
+        font-awesome
+        dejavu_fonts
+        noto-fonts
+        noto-fonts-color-emoji
+        noto-fonts-cjk-serif
+        noto-fonts-cjk-sans
+        source-code-pro
+        open-sans
+        liberation_ttf
+        roboto
+      ];
     };
-    packages = with pkgs-stable; [
-      nerd-fonts.iosevka
-      nerd-fonts.iosevka-term
-      nerd-fonts.ubuntu-sans
-      nerd-fonts.sauce-code-pro
-      nerd-fonts.jetbrains-mono
-      font-awesome
-      dejavu_fonts
-      noto-fonts
-      noto-fonts-color-emoji
-      noto-fonts-cjk-serif
-      noto-fonts-cjk-sans
-      source-code-pro
-      open-sans
-      liberation_ttf
-      roboto
-    ];
   };
 }
