9ea45de1ea1568eeaa49fc401487b78b45cbef23
Artur Sannikov <git-sign@asannikov.com>
2026-06-10 22:27:27 +0300

    feat(dendritic): move system packages

--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -1,58 +1,61 @@
 {
-  pkgs,
-  pkgs-stable,
-  ...
-}:
-# Install system-wide packages
-{
-  environment.systemPackages =
-    with pkgs;
-    [
-      p7zip
-      aha
-      android-tools
-      cabextract
-      clinfo
-      cyrus-sasl-xoauth2 # Required for mbsync email sync for outlook
-      dig
-      dmidecode
-      exfat
-      exfatprogs
-      gcc
-      git
-      gparted
-      inxi
-      kdePackages.isoimagewriter
-      koboldcpp
-      libva-utils
-      lm_sensors
-      mesa-demos
-      mono
-      nano
-      nfs-utils
-      nixd
-      nix-init
-      nmap
-      pciutils
-      pv
-      python313
-      samba
-      sbctl
-      stress-ng
-      system-config-printer
-      unzip
-      usbutils
-      vim
-      vulkan-tools
-      wayland-utils
-      wget
-      wireguard-tools
-    ]
-    ++ (with pkgs-stable; [
-      quickemu
-    ]);
+  flake.nixosModules.systemPackages =
+    {
+      pkgs,
+      pkgs-stable,
+      ...
+    }:
+    # Install system-wide packages
+    {
+      environment.systemPackages =
+        with pkgs;
+        [
+          p7zip
+          aha
+          android-tools
+          cabextract
+          clinfo
+          cyrus-sasl-xoauth2 # Required for mbsync email sync for outlook
+          dig
+          dmidecode
+          exfat
+          exfatprogs
+          gcc
+          git
+          gparted
+          inxi
+          kdePackages.isoimagewriter
+          koboldcpp
+          libva-utils
+          lm_sensors
+          mesa-demos
+          mono
+          nano
+          nfs-utils
+          nixd
+          nix-init
+          nmap
+          pciutils
+          pv
+          python313
+          samba
+          sbctl
+          stress-ng
+          system-config-printer
+          unzip
+          usbutils
+          vim
+          vulkan-tools
+          wayland-utils
+          wget
+          wireguard-tools
+        ]
+        ++ (with pkgs-stable; [
+          quickemu
+        ]);
 
-  programs = {
-    partition-manager.enable = true;
-  };
+      programs = {
+        partition-manager.enable = true;
+      };
+    };
 }
