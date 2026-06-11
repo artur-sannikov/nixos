2a867e59d2bf2fbd661233468a9bc0d37807aa15
Artur Sannikov <git-sign@asannikov.com>
2026-06-11 15:34:33 +0300

    feat(dendritic): migrate syncthing

--- modules/system/services/syncthing.nix
+++ modules/system/services/syncthing.nix
@@ -1,24 +1,28 @@
 { username, ... }:
 {
-  services.syncthing = {
-    user = username;
-    enable = true;
-    configDir = "/home/${username}/.config/syncthing";
-    # Not declarative as of now
-    overrideDevices = false;
-    overrideFolders = false;
-    # See https://docs.syncthing.net/users/config.html#config-file-format
-    settings = {
-      gui = {
-        theme = "dark";
-      };
-      options = {
-        # Use Tailscale instead of relays
-        natEnabled = true;
-        relaysEnabled = true;
-        globalAnnounceEnabled = true;
-        localAnnounceEnabled = true;
-        urAccepted = -1;
+  flake.modules.nixosModules.syncthing = {
+    services = {
+      syncthing = {
+        user = username;
+        enable = true;
+        configDir = "/home/${username}/.config/syncthing";
+        # Not declarative as of now
+        overrideDevices = false;
+        overrideFolders = false;
+        # See https://docs.syncthing.net/users/config.html#config-file-format
+        settings = {
+          gui = {
+            theme = "dark";
+          };
+          options = {
+            # Use Tailscale instead of relays
+            natEnabled = true;
+            relaysEnabled = true;
+            globalAnnounceEnabled = true;
+            localAnnounceEnabled = true;
+            urAccepted = -1;
+          };
+        };
       };
     };
   };
