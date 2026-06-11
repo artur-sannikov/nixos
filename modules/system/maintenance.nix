3c8edc307c2a81409036cf63753f29ec2275ce69
Artur Sannikov <git-sign@asannikov.com>
2026-06-11 14:09:58 +0300

    feat(dendritic): move maintenance

--- modules/system/maintenance.nix
+++ modules/system/maintenance.nix
@@ -1,24 +1,26 @@
 {
-  system = {
-    autoUpgrade = {
-      # Managed via flake.lock
-      enable = false;
+  flake.modules.nixosModules.nix-maintenance = {
+    system = {
+      autoUpgrade = {
+        # Managed via flake.lock
+        enable = false;
+      };
     };
-  };
-  nix = {
-    settings = {
-      # Runs at rebuild, slow
-      auto-optimise-store = false;
-    };
-    # Hard link identical files in nix store
-    optimise = {
-      automatic = true;
-      dates = [ "18:00" ];
-    };
-    gc = {
-      automatic = true;
-      dates = [ "weekly" ];
-      options = "--delete-older-than 30d";
+    nix = {
+      settings = {
+        # Runs at rebuild, slow
+        auto-optimise-store = false;
+      };
+      # Hard link identical files in nix store
+      optimise = {
+        automatic = true;
+        dates = [ "18:00" ];
+      };
+      gc = {
+        automatic = true;
+        dates = [ "weekly" ];
+        options = "--delete-older-than 30d";
+      };
     };
   };
 }
