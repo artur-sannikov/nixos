6292ce5c17ea75abcab44e8259b2823efec4214d
Artur Sannikov <git-sign@asannikov.com>
2026-06-11 08:35:35 +0300

    feat(dendritic): move contacts in home-manager

--- modules/home-manager/accounts/contact.nix
+++ modules/home-manager/accounts/contact.nix
@@ -9,57 +9,59 @@ let
   carddav_url = "https://nextcloud.${flake-inputs.nix-secrets.domain}/remote.php/dav/addressbooks/users/${username}";
 in
 {
-  accounts = {
-    contact = {
-      basePath = ".contacts";
-      accounts = {
-        default = {
-          khard = {
-            enable = true;
-            type = "discover"; # Allow multiple address books
-          };
-          pimsync = {
-            enable = true;
-            extraPairDirectives = [
-              {
-                name = "collection";
-                params = [
-                  "work-1"
-                ];
-              }
-            ];
-          };
-          local = {
-            type = "filesystem";
-            fileExt = "vcf";
-          };
-          remote = {
-            passwordCommand = [
-              "${pkgs.coreutils}/bin/cat"
-              config.sops.secrets."pimsync/nextcloud/password".path
-            ];
-            type = "carddav";
-            url = "${carddav_url}/work-1";
-            userName = "${username}";
+  flake.modules.homeManager.contact = {
+    accounts = {
+      contact = {
+        basePath = ".contacts";
+        accounts = {
+          default = {
+            khard = {
+              enable = true;
+              type = "discover"; # Allow multiple address books
+            };
+            pimsync = {
+              enable = true;
+              extraPairDirectives = [
+                {
+                  name = "collection";
+                  params = [
+                    "work-1"
+                  ];
+                }
+              ];
+            };
+            local = {
+              type = "filesystem";
+              fileExt = "vcf";
+            };
+            remote = {
+              passwordCommand = [
+                "${pkgs.coreutils}/bin/cat"
+                config.sops.secrets."pimsync/nextcloud/password".path
+              ];
+              type = "carddav";
+              url = "${carddav_url}/work-1";
+              userName = "${username}";
+            };
           };
         };
       };
     };
-  };
-  programs = {
-    pimsync = {
-      enable = true;
-      settings = [
-        {
-          name = "status_path";
-          params = [ "${config.xdg.dataHome}/pimsync/status" ];
-        }
-      ];
+    programs = {
+      pimsync = {
+        enable = true;
+        settings = [
+          {
+            name = "status_path";
+            params = [ "${config.xdg.dataHome}/pimsync/status" ];
+          }
+        ];
+      };
     };
-  };
-  services = {
-    pimsync = {
-      enable = true;
+    services = {
+      pimsync = {
+        enable = true;
+      };
     };
   };
 }
