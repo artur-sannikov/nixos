e3c8347147cf5b488188ac21f074e11a6c58162d
Artur Sannikov <git-sign@asannikov.com>
2026-06-11 14:09:50 +0300

    feat(dendritic): move locale

--- modules/system/locale.nix
+++ modules/system/locale.nix
@@ -1,10 +1,12 @@
 {
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    supportedLocales = [
-      "en_GB.UTF-8/UTF-8"
-      "en_US.UTF-8/UTF-8"
-      "fi_FI.UTF-8/UTF-8"
-    ];
+  flake.modules.nixosModules.base = {
+    i18n = {
+      defaultLocale = "en_US.UTF-8";
+      supportedLocales = [
+        "en_GB.UTF-8/UTF-8"
+        "en_US.UTF-8/UTF-8"
+        "fi_FI.UTF-8/UTF-8"
+      ];
+    };
   };
 }
