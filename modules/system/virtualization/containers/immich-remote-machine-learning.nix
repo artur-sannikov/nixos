b537b6bb7e288d8d40c9f9276d5073f147369f8d
Artur Sannikov <git-sign@asannikov.com>
2026-06-11 12:52:07 +0300

    feat(dendritic): move immich-machine-learning

--- modules/system/virtualization/containers/immich-remote-machine-learning.nix
+++ modules/system/virtualization/containers/immich-remote-machine-learning.nix
@@ -1,21 +1,5 @@
 {
-  config,
-  lib,
-  ...
-}:
-with lib;
-let
-  cfg = config.modules.system.virtualisation.containers.immich-remote-machine-learning;
-in
-{
-  options.modules.system.virtualisation.containers.immich-remote-machine-learning = {
-    enable = mkOption {
-      type = types.bool;
-      default = false;
-      description = "Enable Immich Remote Machine Learning";
-    };
-  };
-  config = mkIf cfg.enable {
+  flake.modules.nixosModules.immich-machine-learning = {
     virtualisation.oci-containers = {
       backend = "podman";
       containers = {
