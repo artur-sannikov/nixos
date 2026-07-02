dabea2f1f9394ac3e0b2d0018ce4bde014870200
Artur Sannikov <git-sign@asannikov.com>
2026-07-02 14:25:32 +0300

    chore: styling of ratbagd

--- modules/nixos/ratbagd.nix
+++ modules/nixos/ratbagd.nix
@@ -2,9 +2,15 @@
   flake.modules.nixos.ratbagd =
     { pkgs, ... }:
     {
-      services.ratbagd.enable = true;
-      environment.systemPackages = [
-        pkgs.piper
-      ];
+      services = {
+        ratbagd = {
+          enable = true;
+        };
+      };
+      environment = {
+        systemPackages = [
+          pkgs.piper
+        ];
+      };
     };
 }
