472071578fd8df82df21b3940dc5c5a9b98867bf
Artur Sannikov <git-sign@asannikov.com>
2026-07-03 17:49:27 +0300

    refactor: move llama-cpu to laptop

--- modules/profiles/workstation-work.nix
+++ modules/profiles/workstation-work.nix
@@ -2,7 +2,6 @@
 {
   flake.modules.nixos.workstation-work = {
     imports = with inputs.self.modules.nixos; [
-      llama-cpu
       workstation # Common stuff between personal and work
       samba # For printing and stuff
       xremap
