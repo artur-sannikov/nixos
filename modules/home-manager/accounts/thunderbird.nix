e040bfcd01a616795adb1af7125ceeb32bc72c1e
Artur Sannikov <git-sign@asannikov.com>
2026-07-07 09:40:40 +0300

    feat(thunderbird): declare more settings
    feat(thunderbird): disable chat
    
    feat(thunderbird): disable check for default email client
    
    feat(thunderbird): enable Global Search and Indexer
    
    feat(thunderbird): add more additional settings

--- modules/home-manager/accounts/thunderbird.nix
+++ modules/home-manager/accounts/thunderbird.nix
@@ -1,43 +1,65 @@
 {
-  flake.modules.homeManager.email = { pkgs, ... }: {
-    accounts = {
-      email = {
-        accounts = {
-          work = {
-            thunderbird = {
-              enable = true;
-              profiles = [ "work" ];
-              settings = id: {
-                "mail.server.server_${id}.authMethod" = 3;
+  flake.modules.homeManager.email =
+    { pkgs, ... }:
+    let
+      commonSettings = {
+        # Do not accept third-party cookies
+        "network.cookie.cookieBehavior" = 1;
+
+        # Do not sell or share data
+        "privacy.globalprivacycontrol.enabled" = true;
+        "privacy.donottrackheader.enabled" = true;
+
+        # Disable remote contente
+        "mailnews.message_display.disable_remote_image" = true;
+      };
+    in
+    {
+      accounts = {
+        email = {
+          accounts = {
+            work = {
+              thunderbird = {
+                enable = true;
+                profiles = [ "work" ];
+                settings = id: {
+                  "mail.server.server_${id}.authMethod" = 3;
+                };
+              };
+            };
+            migadu = {
+              thunderbird = {
+                enable = true;
+                profiles = [ "personal" ];
               };
             };
           };
-          migadu = {
-            thunderbird = {
-              enable = true;
-              profiles = [ "personal" ];
+        };
+      };
+      programs = {
+        thunderbird = {
+          enable = true;
+          package = pkgs.thunderbird-latest;
+          profiles = {
+            work = {
+              isDefault = true;
+              settings = commonSettings;
+            };
+            personal = {
+              isDefault = false;
+              settings = commonSettings;
             };
           };
+          settings = {
+            "privacy.donottrackheader.enabled" = true;
+            "dom.event.clipboardevents.enabled" = false;
+            "mail.chat.enabled" = false;
+            "mail.shell.checkDefaultClient" = false;
+
+            # Enable Global Search and Indexer in settings
+            "mailnews.database.global.indexer.enabled" = true;
+          };
         };
       };
     };
-    programs = {
-      thunderbird = {
-        enable = true;
-        package = pkgs.thunderbird-latest;
-        profiles = {
-          work = {
-            isDefault = true;
-          };
-          personal = {
-            isDefault = false;
-          };
-        };
-        settings = {
-          "privacy.donottrackheader.enabled" = true;
-          "dom.event.clipboardevents.enabled" = false;
-        };
-      };
-    };
-  };
 }
